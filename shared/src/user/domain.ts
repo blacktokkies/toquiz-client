6cd06c9e1c13ed9422b6c59564ef1c0eb11ec445
leegwae <leegwae@gmail.com>
2023-01-29 14:42:36 +0900

    feat: API 응답 타입을 정의한다 (#3)
    - login
    - logout
    - signout
    - signup

---
+++ shared/src/user/domain.ts
@@ -0,0 +1,5 @@
+export interface User {
+  id: string;
+  pw: string;
+  nickname: string;
+}
---
+++ shared/src/user/logout.ts
@@ -0,0 +1,7 @@
+import { SuccessResponse } from '../response';
+
+export interface LogOutResult {
+  message: string;
+}
+
+export type LogOutResponse = SuccessResponse<LogOutResult>;
