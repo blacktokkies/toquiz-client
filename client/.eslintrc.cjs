02fe7ba7991299ef6c449c52af1ac544fb50019b
leegwae <leegwae@gmail.com>
2023-02-02 03:25:21 +0900

    setting: `client` 패키지에 ESLint, Prettier을 설정한다 (#9)
    - ESLint, Prettier 린트 규칙을 설정한다.
    - `lint` npm script를 작성한다.

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  globals: {
+    JSX: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'standard-with-typescript',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    'public',
+    'dist',
+    '**/*.config.ts',
+    '**/*.config.js',
+  ],
+  rules: {
+    '@typescript-eslint/triple-slash-reference': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
