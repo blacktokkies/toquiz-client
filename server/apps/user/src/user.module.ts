da6d1c30bf395f0747c55ec755617975d5ca15d8
soomanbaek <qortnaks311@gmail.com>
2023-02-01 20:51:11 +0900

    feat: 생성한 prismaService 모듈에 추가 (#8)

--- server/apps/user/src/user.module.ts
+++ server/apps/user/src/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'libs/prisma.service';
+import { MysqlPrismaService, MongodbPrismaService } from 'libs/prisma-service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, MysqlPrismaService, MongodbPrismaService],
 })
 export class UserModule {}
