d0aabadda6a60f891f048423c1387a139d2fb974
soomanbaek <qortnaks311@gmail.com>
2023-02-01 15:25:08 +0900

    feat: prisma service 생성 및 panel, user 모듈에 import (#8)

--- server/apps/panel/src/panel.module.ts
+++ server/apps/panel/src/panel.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'libs/prisma.service';
 import { PanelController } from './panel.controller';
 import { PanelService } from './panel.service';
 
 @Module({
   imports: [],
   controllers: [PanelController],
-  providers: [PanelService],
+  providers: [PanelService, PrismaService],
 })
 export class PanelModule {}
