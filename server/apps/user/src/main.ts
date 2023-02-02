e2dd38666caba22cef5857fde3fc418554674330
soomanbaek <qortnaks311@gmail.com>
2023-02-02 13:07:51 +0900

    feat: NestJS Swagger 연동

--- server/apps/user/src/main.ts
+++ server/apps/user/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { setSwagger } from 'libs/swagger-setting';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+  setSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
