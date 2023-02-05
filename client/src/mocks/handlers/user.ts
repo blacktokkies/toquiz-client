4c77e1e62addab4394bb8389f1043d53fd5a9eb8
leegwae <leegwae@gmail.com>
2023-02-06 03:33:47 +0900

    setting: API 모킹을 위해 MSW를 설치/설정한다 (#9)
    - MSW를 설치한다.
    - Type-Safe한 모킹 예시로 `GET /api/user` 핸들러를 작성한다.

---
+++ client/src/mocks/handlers/user.ts
@@ -0,0 +1,21 @@
+import { rest } from 'msw';
+
+interface GetUserRequestBody {
+  id: string;
+}
+
+interface GetUserRequestParams {
+  myParam: string;
+}
+
+interface GetUserResponseBody {
+  name: string;
+}
+
+export const getUser = rest.get<
+  GetUserRequestBody,
+  GetUserRequestParams,
+  GetUserResponseBody
+>('/api/user', (req, res, ctx) => {
+  return res(ctx.json({ name: '사용자' }));
+});
