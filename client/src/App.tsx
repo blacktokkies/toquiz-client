c694f1c211d5b4b4cf37439e9a4ce6338036b7d2
leegwae <leegwae@gmail.com>
2023-02-06 04:10:10 +0900

    setting: React Query를 설치하고 `queryClient`를 생성한다 (#9)
    - React Query를 설치한다.
    - 쿼리 클라이언트 인스턴스를 생성한다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import React from 'react';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App(): JSX.Element {
-  return <div>App</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>App</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
