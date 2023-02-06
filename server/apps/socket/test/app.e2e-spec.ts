bfc4a54dc73e462b62fba20009f1598087fdc215
soomanbaek <qortnaks311@gmail.com>
2023-02-06 16:36:13 +0900

    setting: server 구조 변경 (#1)
    - user, panel -> api, socket

--- server/apps/socket/test/app.e2e-spec.ts
+++ server/apps/socket/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { UserModule } from '../src/user.module';
+import { SocketModule } from '../src/socket.module';
 
-describe('UserController (e2e)', () => {
+describe('SocketController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [UserModule],
+      imports: [SocketModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,9 +16,6 @@ describe('UserController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
