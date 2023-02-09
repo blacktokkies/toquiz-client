44ee3ef21f89bedd20b499d95a2df22e3c9c2c6b
leegwae <leegwae@gmail.com>
2023-02-10 08:14:44 +0900

    design: 기초 CSS를 설정하고 Tailwind 상수를 정의한다

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,38 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      xs: '475px',
+      sm: '640px',
+      md: '768px',
+      xl: '1200px',
+    },
+    colors: {
+      white: '#FFFFFF',
+      'off-white': '#F8F8F8',
+      black: '#000000',
+      'off-black': '#242424',
+      primary: {
+        light: '#ABE1FF',
+        DEFAULT: '#2DB3FF',
+        dark: '#0C3045',
+      },
+      error: {
+        dark: '#F42828',
+      },
+      success: {
+        dark: '#198E48',
+        light: '#E2FEED',
+      },
+      grey: {
+        light: '#ECE3E3',
+        DEFAULT: '#B3B3B3',
+        dark: '#656060',
+      },
+    },
     extend: {},
   },
   plugins: [],
