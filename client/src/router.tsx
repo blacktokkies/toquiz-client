0cc2bd75942c9751ba63e47019db8e8bcebed4bf
leegwae <leegwae@gmail.com>
2023-02-10 13:08:22 +0900

    feat: 라우터에 회원가입 페이지를 등록한다 (#17)

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -4,11 +4,20 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Error from '@/pages/Error';
 import Home from '@/pages/Home';
+import SignUp from '@/pages/SignUp';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     errorElement: <Error />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '/login',
+    element: <div>login</div>,
   },
 ]);
