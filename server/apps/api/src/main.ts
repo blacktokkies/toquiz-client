d0997f201decb6b4d72de1716daadf27a9238575
soomanbaek <qortnaks311@gmail.com>
2023-02-10 20:50:25 +0900

    setting: 환경 변수 받아오는 방식 수정 (#1)
    - dotenv -> ConfigService

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { setSwagger } from 'libs/swagger-setting';
 import { winstonLogger } from 'libs/wisnton-logger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule, {
     logger: winstonLogger,
   });
   setSwagger(app, 'api-swagger');
+  const config = app.get<ConfigService>(ConfigService);
 
-  await app.listen(process.env.API_PORT || 3000);
+  await app.listen(config.get('API_PORT') || 3000);
 }
 bootstrap();
