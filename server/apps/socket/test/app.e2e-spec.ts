ae4b2e0c6be67a5d72d3224a57daeffc519ee022
soomanbaek <qortnaks311@gmail.com>
2023-02-10 16:13:21 +0900

    setting: e2e 테스트 supertest import 명령어 수정 (#1)

--- server/apps/socket/test/app.e2e-spec.ts
+++ server/apps/socket/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { SocketModule } from '../src/socket.module';
 
 describe('SocketController (e2e)', () => {
