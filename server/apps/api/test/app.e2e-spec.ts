9d0552745c08f6b6999bb838ee42df2fd13bff0a
soomanbaek <qortnaks311@gmail.com>
2023-02-10 16:13:21 +0900

    setting: e2e 테스트 supertest import 명령어 수정 (#1)

--- server/apps/api/test/app.e2e-spec.ts
+++ server/apps/api/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { ApiModule } from '../src/api.module';
 
 describe('ApiController (e2e)', () => {
