c06e4f40650b1a50011eb62cbf81678b9fda5c5d
soomanbaek <qortnaks311@gmail.com>
2023-02-11 10:48:41 +0900

    feat: 사용자 계정 관련 API 요청 인터페이스 수정 (#3)
    - LogInBody, SignUpBody 추가

--- shared/src/api/users/auth/login.ts
+++ shared/src/api/users/auth/login.ts
@@ -1,6 +1,14 @@
 import { type SuccessResponse } from '@api/response';
 import { type User } from '@api/users/domain';
 
-export type LogInResult = Pick<User, 'username' | 'nickname'>;
+export interface LogInBody {
+  username: User['username'];
+  nickname: User['nickname'];
+}
+
+export interface LogInResult {
+  username: User['username'];
+  nickname: User['nickname'];
+}
 
 export type LogInResponse = SuccessResponse<LogInResult>;
--- shared/src/api/users/auth/signup.ts
+++ shared/src/api/users/auth/signup.ts
@@ -1,4 +1,11 @@
 import { type SuccessResponse } from '@api/response';
+import { type User } from '@api/users/domain';
+
+export interface SignUpBody {
+  username: User['username'];
+  password: string;
+  nickname: User['nickname'];
+}
 
 export interface SignUpResult {
   message: string;
