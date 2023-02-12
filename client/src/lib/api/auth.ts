83d04ba9bf0d23a1b3cba8375a3693eafc38c024
leegwae <leegwae@gmail.com>
2023-02-13 00:27:15 +0900

    refactor: Shared 패키지로부터 API 인터페이스를 가져다쓴다 (#20)

--- client/src/lib/api/auth.ts
+++ client/src/lib/api/auth.ts
@@ -1,22 +1,13 @@
+import type { SignUpBody } from 'shared';
+
 import { apiUrl } from '@/lib/constants';
 
 // TODO: 정의한 fetch 모듈로 바꿔야한다
-export const signUp = async (params: SignUpParams): Promise<any> =>
+export const signUp = async (body: SignUpBody): Promise<any> =>
   fetch(apiUrl.auth.signup(), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(params),
+    body: JSON.stringify(body),
   }).then(async (res) => res.json());
-
-export interface User {
-  username: string;
-  nickname: string;
-}
-
-export interface SignUpParams {
-  username: User['username'];
-  password: string;
-  nickname: User['nickname'];
-}
