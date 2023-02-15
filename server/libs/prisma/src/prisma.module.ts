832ec7a6746e3be3725389ff211fcf285d5f1ce4
soomanbaek <qortnaks311@gmail.com>
2023-02-15 14:58:43 +0900

    feat: Prisma 모듈 글로벌 모듈로 설정 (#14)
    - 글로벌 모듈은 루트 또는 코어 모듈에 한 번 등록 필요

--- server/libs/prisma/src/prisma.module.ts
+++ server/libs/prisma/src/prisma.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MysqlPrismaService } from './mysql-prisma.service';
 import { MongodbPrismaService } from './mongodb-prisma.service';
 
+@Global()
 @Module({
   providers: [MysqlPrismaService, MongodbPrismaService],
   exports: [MysqlPrismaService, MongodbPrismaService],
