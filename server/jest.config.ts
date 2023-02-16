93a737087a57e0bb269dc3b460e80a7f26851d44
soomanbaek <qortnaks311@gmail.com>
2023-02-16 17:31:26 +0900

    build: jest 환경 설정 (#1)

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -5,10 +5,11 @@ import { compilerOptions } from './tsconfig.json';
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
-  // [...]
   roots: ['<rootDir>'],
+  testEnvironment: 'node',
   modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
+  preset: 'ts-jest',
 };
 
 export default jestConfig;
