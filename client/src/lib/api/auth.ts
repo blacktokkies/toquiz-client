616152b2e0ebc1ac09b8a7686742c09480fd5c91
leegwae <leegwae@gmail.com>
2023-02-17 05:36:49 +0900

    feat: `POST /api/users/auth/login` 요청 함수를 정의한다 (#28)

--- client/src/lib/api/auth.ts
+++ client/src/lib/api/auth.ts
@@ -1,4 +1,4 @@
-import type { SignUpBody } from 'shared';
+import type { SignUpBody, SuccessResponse } from 'shared';
 
 import { apiUrl } from '@/lib/constants';
 
@@ -11,3 +11,22 @@ export const signUp = async (body: SignUpBody): Promise<any> =>
     },
     body: JSON.stringify(body),
   }).then(async (res) => res.json());
+
+export const login = async (body: LoginBody): Promise<any> =>
+  fetch(apiUrl.auth.login(), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then(async (res) => res.json());
+
+// TODO: shared 패키지 절대 경로 상대 경로로 수정 후 shared 패키지에서 가져오는 것으로 바꾸어야한다
+export interface LoginBody {
+  username: string;
+  password: string;
+}
+export interface LoginResult {
+  nickname: string;
+}
+export type LoginResponse = SuccessResponse<LoginResult>;
