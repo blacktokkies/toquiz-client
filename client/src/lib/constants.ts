d476049c98f55164b1118cd6978f2aea2816e5b3
leegwae <leegwae@gmail.com>
2023-02-17 05:29:42 +0900

    feat: `POST /api/users/auth/login` 엔드포인트를 상수로 정의한다 (#28)

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -9,6 +9,7 @@ export const queryKey = {
 const AUTH_API_BASE_URL = `/api/users/auth`;
 const authApiUrl = {
   signup: () => `${AUTH_API_BASE_URL}/signup`,
+  login: () => `${AUTH_API_BASE_URL}/login`,
 };
 
 export const apiUrl = {
