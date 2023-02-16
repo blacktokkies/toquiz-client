5dcf97f5bf9d0229c0917c716292a09d5542db83
leegwae <leegwae@gmail.com>
2023-02-17 01:29:36 +0900

    feat: 이메일 입력값 검증하는 `isEmail`을 정의한다 (#19)

--- client/src/lib/validator/index.ts
+++ client/src/lib/validator/index.ts
@@ -1,3 +1,6 @@
+import emailValidator from 'validator/lib/isEmail';
+
 export type Validator = (value: string) => boolean;
 
 export const isNickname: Validator = (value) => /^.+$/.test(value.trim());
+export const isEmail: Validator = (value) => emailValidator(value);
