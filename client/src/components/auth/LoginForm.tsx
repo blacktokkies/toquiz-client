ddb9cae2e298c1d41123b25bcb9e217a896f69fc
leegwae <leegwae@gmail.com>
2023-02-17 05:43:19 +0900

    feat: 로그인 페이지 Form을 작성한다 (#28)

---
+++ client/src/components/auth/LoginForm.tsx
@@ -0,0 +1,61 @@
+import type { LoginBody } from '@/lib/api/auth';
+
+import React from 'react';
+
+import Button from '@/components/Button';
+import LabelInput from '@/components/LabelInput';
+import { useFormError } from '@/hooks/useFormError';
+import { isEmail, isPassword } from '@/lib/validator';
+
+interface Props {
+  action: (props: LoginBody) => void;
+}
+const SignUpForm = ({ action }: Props): JSX.Element => {
+  const { inputProps, errors, handleSubmit } = useFormError({
+    form: {
+      username: {
+        validate: (value) => isEmail(value),
+        errorMessage: '이메일 형식의 아이디를 입력하세요',
+      },
+      password: {
+        validate: (value) => isPassword(value),
+        errorMessage:
+          '8~20자 이하의 영문 대소문자, 숫자, 특수기호를 입력하세요',
+      },
+    },
+  });
+
+  const submit = handleSubmit((data) => {
+    const { username, password } = data;
+    action({ username, password });
+  });
+
+  return (
+    <form className="flex w-full flex-col gap-8" onSubmit={submit}>
+      <div className="flex flex-col gap-5">
+        <LabelInput
+          label="아이디"
+          name="username"
+          required
+          placeholder="아이디를 입력하세요"
+          errorMessage={errors.username}
+          {...inputProps.username}
+        />
+        <LabelInput
+          label="비밀번호"
+          name="password"
+          type="password"
+          required
+          placeholder="비밀번호를 입력하세요"
+          errorMessage={errors.password}
+          {...inputProps.password}
+        />
+      </div>
+      <Button type="submit" className="w-full">
+        로그인
+      </Button>
+    </form>
+  );
+};
+
+export default SignUpForm;
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,26 @@
+import type { LoginBody } from '@/lib/api/auth';
+
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import LoginForm from '@/components/auth/LoginForm';
+
+function Login(): JSX.Element {
+  const handleSubmit = ({ username, password }: LoginBody): void => {};
+
+  return (
+    <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center px-9">
+      <div className="mb-14 text-3xl font-bold">toquiz</div>
+      <LoginForm action={handleSubmit} />
+      <div className="mt-2">
+        계정이 없나요?{' '}
+        <Link className="font-bold underline" to="/signup">
+          회원가입
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
