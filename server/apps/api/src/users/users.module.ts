0dd129d5076858356f8d4193950af4b950023826
soomanbaek <qortnaks311@gmail.com>
2023-02-18 00:51:27 +0900

    feat: 로그인 API 로직 (#31)

--- server/apps/api/src/users/users.module.ts
+++ server/apps/api/src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { TokenService } from 'libs/common/authentication/token.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService, UsersRepository, TokenService, JwtService],
 })
 export class UsersModule {}
