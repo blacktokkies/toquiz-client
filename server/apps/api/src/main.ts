ab9a1bb396c5b88e71e755b64d4184349e895186
soomanbaek <qortnaks311@gmail.com>
2023-02-18 00:49:56 +0900

    feat: 쿠키 관련 미들웨어 추가 (#31)

--- server/apps/api/src/main.ts
+++ server/apps/api/src/main.ts
@@ -4,13 +4,15 @@ import { setSwagger } from 'libs/utils/swagger-setting';
 import { winstonLogger } from 'libs/utils/wisnton-logger';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule, {
-    logger: winstonLogger,
+    logger: winstonLogger, // winston Logger 사용
   });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  setSwagger(app, 'api-swagger');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // request validation
+  app.use(cookieParser()); // Cookie 사용
+  setSwagger(app, 'api-swagger'); // Swagger 사용
 
   const config = app.get<ConfigService>(ConfigService);
   const PORT = config.get('API_PORT');
