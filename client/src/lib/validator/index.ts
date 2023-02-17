1e69e9fcb10d06740487f40cc6dfa33dc185d6ea
leegwae <leegwae@gmail.com>
2023-02-17 14:25:58 +0900

    fix: 닉네임 입력값 검증에 길이 제한을 추가한다 (#19)
    - 2자 이상 20자 이하의 문자

--- client/src/lib/validator/index.ts
+++ client/src/lib/validator/index.ts
@@ -2,7 +2,7 @@ import emailValidator from 'validator/lib/isEmail';
 
 export type Validator = (value: string) => boolean;
 
-export const isNickname: Validator = (value) => /^.+$/.test(value.trim());
+export const isNickname: Validator = (value) => /^.{2,20}$/.test(value.trim());
 export const isEmail: Validator = (value) => emailValidator(value);
 export const isPassword: Validator = (value) =>
   /^(?=\S*[a-zA-z])(?=\S*[0-9])(?=\S*[$`~!@$!%*#^?&\\(\\)\-_=+])\S{8,20}$/.test(
