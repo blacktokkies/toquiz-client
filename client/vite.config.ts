79829adbd8a146df055492aeba4a38982ef2bbd7
leegwae <leegwae@gmail.com>
2023-02-21 00:01:43 +0900

    setting: mode가 `proxy`이면 dev server에 API용 프록시를 설정한다 (#37)
    - API 요청의 path가 `/api`로 시작할 때 env에 작성한 `API_ORIGIN`으로 origin을 바꾼다

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    checker({
-      typescript: true,
-      eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const ENV = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      checker({
+        typescript: true,
+        eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+      }),
+    ],
+    server: {
+      proxy: mode === 'proxy' && {
+        '/api': {
+          target: ENV.API_ORIGIN,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
