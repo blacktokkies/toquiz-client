b929ed8f6bb6b7fa02f62e8acc88a47f6d15ca76
soomanbaek <qortnaks311@gmail.com>
2023-02-22 14:37:07 +0900

    feat: jest.config에 env파일 경로 설정 (#31)
    - (https://black-tokkies.notion.site/Jest-TypeError-JwtStrategy-requires-a-secret-or-key-6a9780baa664451bb4199765b096bef5)

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,15 +1,24 @@
+import dotenv from 'dotenv';
+import path from 'path';
 import { pathsToModuleNameMapper } from 'ts-jest';
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
 import { compilerOptions } from './tsconfig.json';
 import type { JestConfigWithTsJest } from 'ts-jest';
 
+// env 경로 설정
+const envPath = path.resolve(__dirname, 'config/.test.env');
+dotenv.config({ path: envPath });
+
 const jestConfig: JestConfigWithTsJest = {
   roots: ['<rootDir>'],
-  testEnvironment: 'node',
-  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   preset: 'ts-jest',
+
+  setupFiles: ['dotenv/config'], // .env 파일의 내용이 환경변수로 등록되어 Jest 에서 사용할 수 있게 됨
+  testEnvironment: 'node', // node 환경에서 실행
+  testEnvironmentOptions: {
+    NODE_ENV: 'test', // 실행 환경은 test
+  },
 };
 
 export default jestConfig;
