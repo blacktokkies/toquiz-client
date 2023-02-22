785e9436f21a7d534ccc3c48b6de36a4fd92a4a7
soomanbaek <qortnaks311@gmail.com>
2023-02-22 14:44:43 +0900

    feat: accessToken, refreshToken Guard 구현 (#31)

---
+++ server/libs/common/guards/strategies/jwt-access.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt-access-token') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request) => {
+          return request?.cookies?.accessToken;
+        },
+      ]),
+      secretOrKey: config.get('JWT_ACCESS_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+}
