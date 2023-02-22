59d5fc5035aa21385512254671c5dd207ce76b9a
soomanbaek <qortnaks311@gmail.com>
2023-02-22 15:05:52 +0900

    feat: user 도메인에 정보 추가 (#3)
    - 테이블에 저장된 User와 일치 시킴

--- shared/src/libs/domains/user.ts
+++ shared/src/libs/domains/user.ts
@@ -1,5 +1,10 @@
 export interface User {
+  id: string;
   username: string;
   password: string;
   nickname: string;
+  provider: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
 }
