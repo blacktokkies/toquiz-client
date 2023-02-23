05dc577d170c67770f9dca4f2089e30dc422cead
soomanbaek <qortnaks311@gmail.com>
2023-02-23 14:24:08 +0900

    feat: 패널 생성 로직 (#43)

---
+++ server/apps/api/src/panels/panels.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePanelDto } from '@api/src/panels/dto';
+import { Panel } from 'shared';
+import { PanelsRepository } from '@api/src/panels/panels.repository';
+
+@Injectable()
+export class PanelsService {
+  constructor(private panelsRepository: PanelsRepository) {}
+
+  async createPanel(createPanelDto: CreatePanelDto): Promise<Panel> {
+    return await this.panelsRepository.createPanel(createPanelDto);
+  }
+}
