98905fd5fbaa9cd942acbaf86589299df2acb449
soomanbaek <qortnaks311@gmail.com>
2023-02-23 13:42:11 +0900

    fix: shared CreatePanelBody가 title, description 을 포함 (#3)

--- shared/src/api/panels/create-panel.ts
+++ shared/src/api/panels/create-panel.ts
@@ -1,9 +1,9 @@
-import type { User, Panel } from '../../libs';
+import type { Panel } from '../../libs';
 import type { SuccessResponse } from '../response';
 
 export interface CreatePanelBody {
-  username: User['username'];
-  password: User['password'];
+  title: Panel['title'];
+  description: Panel['title'];
 }
 
 export interface CreatePanelResult {
