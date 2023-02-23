9ec288f525c4d36b8a7ffa39648a4252f92f69ad
soomanbaek <qortnaks311@gmail.com>
2023-02-23 15:56:10 +0900

    feat: 패널 정보 가져오기 로직 (#46)

--- server/apps/api/src/panels/panels.repository.ts
+++ server/apps/api/src/panels/panels.repository.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { MysqlPrismaService } from 'libs/prisma/src/mysql-prisma.service';
-import { CreatePanelDto } from '@api/src/panels/dto';
 import { Panel } from 'shared';
+import { Prisma } from 'libs/prisma/prisma/generated/mysql';
+import { CreatePanelDto } from '@api/src/panels/dto';
 
 @Injectable()
 export class PanelsRepository {
@@ -9,5 +10,9 @@ export class PanelsRepository {
 
   async createPanel(createPanelDto: CreatePanelDto): Promise<Panel> {
     return await this.mysqlService.panel.create({ data: createPanelDto });
+  }
+
+  async findPanel(panelWhereInput: Prisma.PanelWhereInput): Promise<Panel> {
+    return await this.mysqlService.panel.findFirst({ where: { ...panelWhereInput } });
   }
 }
--- server/apps/api/src/panels/panels.service.ts
+++ server/apps/api/src/panels/panels.service.ts
@@ -10,4 +10,8 @@ export class PanelsService {
   async createPanel(createPanelDto: CreatePanelDto): Promise<Panel> {
     return await this.panelsRepository.createPanel(createPanelDto);
   }
+
+  async getPanel(panelId: Panel['id']): Promise<Panel> {
+    return await this.panelsRepository.findPanel({ id: panelId });
+  }
 }
