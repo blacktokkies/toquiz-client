914295632c1705b039bbb9842dae2faba9e3c58b
leegwae <leegwae@gmail.com>
2023-03-02 00:54:33 +0900

    feat: `/main` 렌더링 전 로그인 상태를 검사하여 리다이렉트한다 (#54)

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,9 +1,21 @@
 import type { User } from '@/store/userStore';
+import type { LoaderFunction } from 'react-router-dom';
 
 import React from 'react';
 
+import { redirect } from 'react-router-dom';
 
 import { useUserStore } from '@/hooks/store/useUserStore';
+import { getUser } from '@/store/userStore';
+
+// https://reactrouter.com/en/main/fetch/redirect
+export const mainLoader: LoaderFunction = ({ request }) => {
+  const user = getUser();
+
+  if (!user) return redirect('/login?from=/main');
+  return null;
+};
+
 const Main = (): JSX.Element => {
   const user = useUserStore((state) => state.user) as User;
   return <div>{JSON.stringify(user)}</div>;
