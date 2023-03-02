b52bc4ee52f00c9c3f79ba27eb475462c9797d93
leegwae <leegwae@gmail.com>
2023-03-03 07:25:29 +0900

    feat: `GET /api/user` 엔드포인트를 상수로 정의한다 (#54)

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -12,6 +12,7 @@ const authApiUrl = {
   signup: () => `${AUTH_API_BASE_URL}/signup` as const,
   login: () => `${AUTH_API_BASE_URL}/login` as const,
   me: () => `/api/user` as const,
+  refresh: () => `${AUTH_API_BASE_URL}/refresh` as const,
 };
 
 export const apiUrl = {
