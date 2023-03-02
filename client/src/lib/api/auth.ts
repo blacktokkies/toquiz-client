172c9f35f0880bf0a2352e08097c23dc8d840fad
leegwae <leegwae@gmail.com>
2023-03-03 07:27:23 +0900

    feat: `POST /api/users/auth/refresh` API 요청 함수를 정의한다 (#54)

--- client/src/lib/api/auth.ts
+++ client/src/lib/api/auth.ts
@@ -6,6 +6,8 @@ import type {
   LogInBody,
   LogInResult,
   LogInResponse,
+  RefreshResult,
+  RefreshResponse,
 } from 'shared';
 
 import { apiClient } from '@/lib/apiClient';
@@ -19,4 +21,14 @@ export const signUp = async (body: SignUpBody): Promise<SignUpResult> =>
 export const login = async (body: LogInBody): Promise<LogInResult> =>
   apiClient
     .post<LogInResponse>(apiUrl.auth.login(), body)
+    .then((data) => data.result);
+
+export const me = async (): Promise<GetMyInfoResult> =>
+  apiClient
+    .get<GetMyInfoResponse>(apiUrl.auth.me())
+    .then((data) => data.result);
+
+export const refresh = async (): Promise<RefreshResult> =>
+  apiClient
+    .post<RefreshResponse>(apiUrl.auth.refresh())
     .then((data) => data.result);
