2d824b75b147227ad28dfcaeb70f21322ab668f9
leegwae <leegwae@gmail.com>
2023-03-03 07:16:46 +0900

    feat: 인덱스 라우트를 추가한다

---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Index = (): JSX.Element => (
+  <div>
+    <Link to="/login" className="border">
+      로그인
+    </Link>
+    <Link to="/signup" className="border">
+      회원가입
+    </Link>
+    <Link to="/main" className="border">
+      메인
+    </Link>
+  </div>
+);
+
+export default Index;
