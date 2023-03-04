16bf5fb43c4696b5ae5ce89d5718f9b6dba0d985
leegwae <leegwae@gmail.com>
2023-03-04 23:10:53 +0900

    feat: API 요청을 담당하는 fetch 모듈을 정의한다 (#63)
    * feat: 로컬 변수로 액세스 토큰을 관리한다 (#22)
    
    * feat: HTTP 오류 응답을 처리할 `ApiError` 클래스를 정의한다 (#22)
    
    * feat: fetch로 HTTP 오류 응답을 받으면 `ApiError`를 발생시키는 함수를 정의한다 (#22)
    
    * feat: API 요청할 fetch 모듈을 정의한다 (#22)
    
    - get, post, delete, patch, put 지원한다
    - HTTP 오류 응답을 받으면 `ApiError`를 발생시킨다

---
+++ client/src/lib/apiClient.ts
@@ -0,0 +1,103 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment */
+let _accessToken = '';
+
+export const setToken = (token: string): void => {
+  _accessToken = token;
+};
+export const clearToken = (): void => {
+  _accessToken = '';
+};
+
+export class ApiError extends Error {
+  constructor(public response: Response, public data: any = undefined) {
+    super(`status ${response.status}: Failed to fetch`);
+  }
+}
+
+async function handleResponse<T>(response: Response): Promise<T> {
+  const data: Promise<T> = response.json();
+  if (response.ok) return data;
+  return Promise.reject(new ApiError(response, data));
+}
+
+export async function request<T>(
+  method: string,
+  url: string,
+  options: RequestInit | undefined = {},
+): Promise<T> {
+  const response = await fetch(url, { method, ...options });
+  return handleResponse(response);
+}
+
+export const apiClient = {
+  async get<T>(url: string, headers: HeadersInit | undefined = {}): Promise<T> {
+    const _headers: HeadersInit = {
+      Authorization: `Bearer ${_accessToken}`,
+      ...headers,
+    };
+    const data = request<T>('GET', url, { headers: _headers });
+    return data;
+  },
+  async post<T>(
+    url: string,
+    body: Record<string, any> | undefined = undefined,
+    headers: HeadersInit | undefined = {},
+  ): Promise<T> {
+    const _headers: HeadersInit = {
+      Authorization: `Bearer ${_accessToken}`,
+      ...(body ? { 'Content-Type': 'application/json' } : {}),
+      ...headers,
+    };
+    const _body = body && JSON.stringify(body);
+    const data: Promise<T> = request('POST', url, {
+      headers: _headers,
+      body: _body,
+    });
+    return data;
+  },
+  async delete<T>(
+    url: string,
+    headers: HeadersInit | undefined = {},
+  ): Promise<T> {
+    const _headers: HeadersInit = {
+      Authorization: `Bearer ${_accessToken}`,
+      ...headers,
+    };
+    const data = request<T>('DELETE', url, { headers: _headers });
+    return data;
+  },
+  async patch<T>(
+    url: string,
+    body: Record<string, any> | undefined = undefined,
+    headers: HeadersInit | undefined = {},
+  ): Promise<T> {
+    const _headers: HeadersInit = {
+      Authorization: `Bearer ${_accessToken}`,
+      ...(body ? { 'Content-Type': 'application/json' } : {}),
+      ...headers,
+    };
+    const _body = body && JSON.stringify(body);
+    const data: Promise<T> = request('PATCH', url, {
+      headers: _headers,
+      body: _body,
+    });
+    return data;
+  },
+  async put<T>(
+    url: string,
+    body: Record<string, any> | undefined = undefined,
+    headers: HeadersInit | undefined = {},
+  ): Promise<T> {
+    const _headers: HeadersInit = {
+      Authorization: `Bearer ${_accessToken}`,
+      ...(body ? { 'Content-Type': 'application/json' } : {}),
+      ...headers,
+    };
+    const _body = body && JSON.stringify(body);
+    const data: Promise<T> = request('PUT', url, {
+      headers: _headers,
+      body: _body,
+    });
+    return data;
+  },
+};
