80d08cdf8ea25a8cfc2d27e229632a0c91e83d2b
soomanbaek <72093196+soomanbaek@users.noreply.github.com>
2023-03-05 15:03:25 +0900

    feat: 자기 자신의 정보를 가져오는 API (#69)
    * feat: 자기 자신의 정보를 가져오는 API
    
    * fix: 자기 자신의 정보에서 비밀번호 지움

---
+++ server/apps/api/src/users/user.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { UsersService } from '@users/users.service';
+import { JwtAccessGuard } from 'libs/common/guards/jwt-access.guard';
+import { User } from 'shared';
+import { Request } from 'express';
+
+@Controller('api/user')
+export class UserController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  @UseGuards(JwtAccessGuard)
+  async getUserInfo(@Req() req: Request): Promise<User> {
+    const user = await this.usersService.getUser({ id: req.user['userId'] });
+    delete user.password;
+
+    return user;
+  }
+}
