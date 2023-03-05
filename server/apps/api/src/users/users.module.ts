76824c14f64a93ffdf86020843c136728737e539
soomanbaek <qortnaks311@gmail.com>
2023-03-05 14:59:50 +0900

    feat: 자기 자신의 정보를 가져오는 API

--- server/apps/api/src/users/users.module.ts
+++ server/apps/api/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { AuthModule } from 'libs/common/authorization/auth.module';
+import { UserController } from '@users/user.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [UsersController],
+  controllers: [UserController, UsersController],
   providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
