6be544b85228e164892641dc14682c8995dd9737
soomanbaek <qortnaks311@gmail.com>
2023-03-06 16:31:48 +0900

    feat: redis 모듈 적용

--- server/apps/api/src/api.module.ts
+++ server/apps/api/src/api.module.ts
@@ -1,15 +1,22 @@
 import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from 'libs/common/middlewares/logger.middleware';
 import { UsersModule } from '@users/users.module';
 import { PrismaModule } from 'libs/prisma/src';
 import { PanelsModule } from '@api/src/panels/panels.module';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { RedisConfigService } from 'libs/common/cache/cache.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.{process.env.NODE_ENV}.env`,
       isGlobal: true,
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: RedisConfigService,
+      inject: [ConfigService],
     }),
     PrismaModule,
     UsersModule,
