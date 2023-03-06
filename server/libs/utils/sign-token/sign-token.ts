b1eddfc8e71108a9645773603bcd2e79ed8d5bf0
soomanbaek <72093196+soomanbaek@users.noreply.github.com>
2023-03-06 16:35:17 +0900

    feat: redis 설정 및 refreshToken 검증 로직 (#71)
    * build: redis 사용을 위한 의존성 추가
    
    * feat: redis를 캐시로 사용하기 위한 모듈 생성
    
    * feat: redis 모듈 적용
    
    * feat: refreshToken 저장 및 검증

--- server/libs/utils/sign-token/sign-token.ts
+++ server/libs/utils/sign-token/sign-token.ts
@@ -3,10 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { User } from 'shared';
 import { Injectable } from '@nestjs/common';
 import { tokenOption } from './token-option';
+import { CacheService } from 'libs/common/cache/cache.service';
 
 @Injectable()
 export class SignToken {
-  constructor(private jwt: JwtService, private config: ConfigService) {}
+  constructor(
+    private jwt: JwtService,
+    private config: ConfigService,
+    private cacheService: CacheService,
+  ) {}
 
   signAccessToken(id: User['id'], nickname: User['nickname']): string {
     const payload = {
@@ -20,6 +25,12 @@ export class SignToken {
     const payload = {
       id,
     };
-    return this.jwt.sign(payload, tokenOption.refreshToken(this.config.get('JWT_REFRESH_SECRET')));
+    const refreshToken = this.jwt.sign(
+      payload,
+      tokenOption.refreshToken(this.config.get('JWT_REFRESH_SECRET')),
+    );
+    this.cacheService.set(id, refreshToken, 60 * 60 * 24 * 14); // 14일 동안 유지
+
+    return refreshToken;
   }
 }
