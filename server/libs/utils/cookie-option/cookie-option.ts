587439a0536605c0c41a3138c48338c7eca09bc0
soomanbaek <qortnaks311@gmail.com>
2023-03-14 14:24:33 +0900

    feat: toquiz-token 옵션

--- server/libs/utils/cookie-option/cookie-option.ts
+++ server/libs/utils/cookie-option/cookie-option.ts
@@ -10,4 +10,8 @@ export const cookieOption = {
     secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 14, // 14일 동안 유지
   },
+  toquizToken: {
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 365, // 1년 동안 유지
+  },
 };
