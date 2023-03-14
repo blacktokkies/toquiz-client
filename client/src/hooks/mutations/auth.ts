753c4705dc3e8333569a7acb965a9ab25bff4318
leegwae <leegwae@gmail.com>
2023-03-14 23:59:14 +0900

    feat: `useMutatioin`에서 `Error` 타이핑을 `ApiError | SyntaxError`으로 좁힌다 (#76)
    * feat: `useMutatioin`에서 `Error` 타이핑을 `ApiError | SyntaxError`으로 좁힌다 (#74)
    
    * refactor: `onMutate` 옵션을 사용하지 않으므로 `unkown` 타이핑을 제거한다

--- client/src/hooks/mutations/auth.ts
+++ client/src/hooks/mutations/auth.ts
@@ -1,3 +1,4 @@
+import type { ApiError } from '@/lib/apiClient';
 import type { UseMutationResult } from '@tanstack/react-query';
 import type { LogInBody, LogInResult, SignUpBody, SignUpResult } from 'shared';
 
@@ -9,26 +10,25 @@ import { queryKey } from '@/lib/constants';
 // TODO: Error 타입은 Api Error랑 error instanceof SyntaxError
 export const useSignUpMutation = (): UseMutationResult<
   SignUpResult,
-  Error,
-  SignUpBody,
-  unknown
+  ApiError | SyntaxError,
+  SignUpBody
 > => {
   const key = queryKey.auth.signup();
-  const mutation = useMutation<SignUpResult, Error, SignUpBody, unknown>(
-    key,
-    signUp,
-  );
+  const mutation = useMutation<
+    SignUpResult,
+    ApiError | SyntaxError,
+    SignUpBody
+  >(key, signUp);
   return mutation;
 };
 
 export const useLoginMutation = (): UseMutationResult<
   LogInResult,
-  Error,
-  LogInBody,
-  unknown
+  ApiError | SyntaxError,
+  LogInBody
 > => {
   const key = queryKey.auth.login();
-  const mutation = useMutation<LogInResult, Error, LogInBody, unknown>(
+  const mutation = useMutation<LogInResult, ApiError | SyntaxError, LogInBody>(
     key,
     login,
   );
