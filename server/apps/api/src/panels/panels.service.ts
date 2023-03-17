294950d4134fc9b17e1881f05a3458876f83b775
soomanbaek <qortnaks311@gmail.com>
2023-03-17 16:00:55 +0900

    feat: getPanel Interface 구현에 따른 type 수정

--- server/apps/api/src/panels/panels.service.ts
+++ server/apps/api/src/panels/panels.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePanelDto } from '@api/src/panels/dto';
-import { Panel } from 'shared';
+import { GetPanelResult, Panel } from 'shared';
 import { PanelsRepository } from '@api/src/panels/panels.repository';
 
 @Injectable()
@@ -11,7 +11,7 @@ export class PanelsService {
     return await this.panelsRepository.createPanel(createPanelDto);
   }
 
-  async getPanel(panelId: Panel['id']): Promise<Panel> {
+  async getPanel(panelId: Panel['id']): Promise<GetPanelResult> {
     return await this.panelsRepository.findPanel({ id: panelId });
   }
 }
