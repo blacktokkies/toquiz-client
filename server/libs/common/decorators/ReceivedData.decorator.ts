3adff324d47b59ce5bc98b37702774d985cc3715
soomanbaek <qortnaks311@gmail.com>
2023-03-19 15:59:30 +0900

    feat: ReceivedData에서 쿠키의 toquizToken을 받아올 수 있다

--- server/libs/common/decorators/ReceivedData.decorator.ts
+++ server/libs/common/decorators/ReceivedData.decorator.ts
@@ -2,10 +2,13 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const ReceivedData = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
-  return {
+  const result = {
     ...req.body,
     ...req.params,
     ...req.query,
-    userId: req.user.id,
   };
+
+  if (req?.user?.id) result['userId'] = req.user.id;
+  if (req?.cookies?.toquizToken) result['toquizUserId'] = req.cookies.toquizToken;
+  return result;
 });
