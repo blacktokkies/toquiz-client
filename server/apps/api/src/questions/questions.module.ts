ca1705f7b29be8ec60f9c8af7dec76b677286384
soomanbaek <72093196+soomanbaek@users.noreply.github.com>
2023-03-19 20:45:37 +0900

    feat: 패널 질문 가져오기 (#114)
    * feat: cursor 기반 pagination 기법으로 패널 질문 목록 받아오는 함수 생성
    
    - getQuestionsFirstPage: 최초로 패널 질문 받아올 경우 호출 (cursor 없음)
    - getQuestionsNextPage: 이전에 패널 질문을 받은적이 있는 경우 호출 (cursor 있음)
    
    * feat: `getPanelQuestions` 관련 `service` 로직
    
    - repository에서 질문목록 받아옴
    - 받아온 질문 목록을 바탕으로 nextCursor 생성
      - nextCursor: 받아온 질문의 가장 마지막 id
    
    * feat: `getPanelQuestions` 관련 `controller` 로직
    
    * feat: Questions module에 Questions controller 추가

--- server/apps/api/src/questions/questions.module.ts
+++ server/apps/api/src/questions/questions.module.ts
@@ -3,10 +3,11 @@ import { QuestionController } from '@api/src/questions/question.controller';
 import { AuthModule } from 'libs/common/authorization/auth.module';
 import { QuestionsService } from '@api/src/questions/questions.service';
 import { QuestionsRepository } from '@api/src/questions/questions.repository';
+import { QuestionsController } from '@api/src/questions/questions.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [QuestionController],
+  controllers: [QuestionController, QuestionsController],
   providers: [QuestionsService, QuestionsRepository],
 })
 export class QuestionsModule {}
