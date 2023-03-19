4c8d8366986820092780171f2c031adbdf1ee420
soomanbaek <qortnaks311@gmail.com>
2023-03-19 16:33:16 +0900

    refactor: 함수 명 수정 `getUserInfo` -> `getMyInfo`

--- server/apps/api/src/users/user.controller.ts
+++ server/apps/api/src/users/user.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from '@users/users.service';
 import { JwtAccessGuard } from 'libs/common/guards/jwt-access.guard';
 import { Request } from 'express';
-import { GetUserInfoResult } from 'shared';
+import { GetMyInfoResult } from 'shared';
 
 @Controller('api/user')
 export class UserController {
@@ -10,7 +10,7 @@ export class UserController {
 
   @Get()
   @UseGuards(JwtAccessGuard)
-  async getUserInfo(@Req() req: Request): Promise<GetUserInfoResult> {
+  async getMyInfo(@Req() req: Request): Promise<GetMyInfoResult> {
     const user = await this.usersService.getUser({ id: req.user['userId'] });
     delete user.password;
 
