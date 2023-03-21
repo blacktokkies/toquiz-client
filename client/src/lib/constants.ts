2b643a8e518cce416e84baea277b7c03c3e49f68
leegwae <leegwae@gmail.com>
2023-03-21 22:03:53 +0900

    refactor: `lib/constant`에서 query key를 분리한다 (#134)

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,12 +1,3 @@
-const authQueryKey = {
-  signup: () => ['signup'] as const,
-  login: () => ['login'] as const,
-} as const;
-
-export const queryKey = {
-  auth: authQueryKey,
-};
-
 const AUTH_API_BASE_URL = `/api/users/auth`;
 const authApiUrl = {
   signup: () => `${AUTH_API_BASE_URL}/signup` as const,
