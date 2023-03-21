4c9f1620722ac9a944ddfca4e019f01e5a67f8ef
soomanbaek <qortnaks311@gmail.com>
2023-03-21 16:31:20 +0900

    feat: 질문 좋아요 관련 `controller`
    - POST `api/panels/:panelId/questions/:questionId/like`

--- server/apps/api/src/questions/questions.controller.ts
+++ server/apps/api/src/questions/questions.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { QuestionsService } from '@api/src/questions/questions.service';
-import { GetPanelQuestionsResult, Question } from 'shared';
+import { GetPanelQuestionsResult, LikeQuestionResult, Question } from 'shared';
+import { ReceivedData } from 'libs/common/decorators/ReceivedData.decorator';
+import { ToquizGuard } from 'libs/common/guards/toquiz.guard';
+import { LikeQuestionDto } from '@api/src/questions/dto';
 
 @Controller('api/panels/:panelId/questions')
 export class QuestionsController {
@@ -12,5 +15,14 @@ export class QuestionsController {
     @Query('cursor') cursor?: Question['id'],
   ): Promise<GetPanelQuestionsResult> {
     return await this.questionsService.getPanelQuestions(panelId, cursor);
+  }
+
+  @Post(':questionId/like')
+  @UseGuards(ToquizGuard)
+  async likeQuestion(
+    @ReceivedData() likeQuestionDto: LikeQuestionDto,
+  ): Promise<LikeQuestionResult> {
+    await this.questionsService.likeQuestion(likeQuestionDto);
+    return { message: 'success to like' };
   }
 }
