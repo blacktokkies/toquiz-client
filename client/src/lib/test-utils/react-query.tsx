beacdf8e4a519306ecef7cbe294bbe1157684fa5
leegwae <leegwae@gmail.com>
2023-03-22 21:04:37 +0900

    feat: React Query 커스텀 훅에 사용할 wrapper 함수를 작성한다 (#136)

--- client/src/lib/test-utils/react-query.tsx
+++ client/src/lib/test-utils/react-query.tsx
@@ -1,4 +1,7 @@
-import { QueryClient } from '@tanstack/react-query';
+/* eslint-disable react/display-name */
+import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // tanstack.com/query/v4/docs/react/guides/testing#turn-off-retries
 // tanstack.com/query/v4/docs/react/guides/testing#turn-off-network-error-logging
@@ -16,3 +19,15 @@ const createQueryClient = (): QueryClient =>
     },
   });
 
+type QueryClientWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => JSX.Element;
+
+export function createQueryClientWrapper(): QueryClientWrapper {
+  const queryClient = createQueryClient();
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
