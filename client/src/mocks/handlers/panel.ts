ee97f9dd63ee191751f97a9075f77305b6f04f23
leegwae <leegwae@gmail.com>
2023-03-24 18:56:08 +0900

    feat: `GET /panels` mock API 핸들러를 작성한다 (#146) (#147)

---
+++ client/src/mocks/handlers/panel.ts
@@ -0,0 +1,25 @@
+import type { Panel, SuccessResponse } from 'shared';
+
+import { rest } from 'msw';
+
+import { API_BASE_URL } from '@/lib/apiClient';
+import { myPanelData } from '@/mocks/data/panel/myPanelData';
+
+export interface GetMyPanelsResult {
+  panels: Panel[];
+}
+export type GetMyPanelsResponse = SuccessResponse<GetMyPanelsResult>;
+
+export const getMyPanels = rest.get<never, never, GetMyPanelsResponse>(
+  `${API_BASE_URL}/panels`,
+  (_, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        statusCode: 200,
+        result: {
+          panels: myPanelData,
+        },
+      }),
+    ),
+);
