13d1043d40971446396b0e1b1f086b4cad8ef712
leegwae <leegwae@gmail.com>
2023-03-24 19:37:11 +0900

    refactor: `useMyPanelsQuery` 쿼리 키를 분리한다 (#145)

--- client/src/lib/queryKey.ts
+++ client/src/lib/queryKey.ts
@@ -3,6 +3,12 @@ const authQueryKey = {
   login: () => ['login'] as const,
 } as const;
 
+const panelQueryKey = {
+  all: ['panels'] as const,
+  list: () => [...panelQueryKey.all, 'list'] as const,
+};
+
 export const queryKey = {
   auth: authQueryKey,
+  panel: panelQueryKey,
 };
