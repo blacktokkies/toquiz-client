df6607c44c3bf78e01ab62cd7841ee0f5ce66a45
leegwae <leegwae@gmail.com>
2023-03-24 19:39:02 +0900

    refactor: `useMyPanelsQuery` 쿼리 키를 분리한다 (#145) (#153)

--- client/src/hooks/panel.ts
+++ client/src/hooks/panel.ts
@@ -1,12 +1,21 @@
+import type { ApiError } from '@/lib/apiClient';
 import type { GetMyPanelsResult } from '@/mocks/handlers/panel';
 import type { UseQueryResult } from '@tanstack/react-query';
 
 import { useQuery } from '@tanstack/react-query';
 
 import { getMyPanels } from '@/lib/api/panel';
+import { queryKey } from '@/lib/queryKey';
 
-export const useMyPanelsQuery = (): UseQueryResult<GetMyPanelsResult> => {
-  const query = useQuery(['panels'], getMyPanels);
+export const useMyPanelsQuery = (): UseQueryResult<
+  GetMyPanelsResult,
+  ApiError | SyntaxError
+> => {
+  const key = queryKey.panel.list();
+  const query = useQuery<GetMyPanelsResult, ApiError | SyntaxError>(
+    key,
+    getMyPanels,
+  );
 
   return query;
 };
