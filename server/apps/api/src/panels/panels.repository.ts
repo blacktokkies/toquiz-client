1036e35ec872b3677a319164e117f8e75c0164bb
soomanbaek <qortnaks311@gmail.com>
2023-03-27 02:12:25 +0900

    refactor: `panel repository`
    - 함수명 변경 `findPanel` -> `getPanelByPanelId`
    - `getPanelsIdOfToquizUser` 생성 (ToquizUser가 활동한 배널들의 id정보)
    - `createPanelOfToquzUser` 생성 (ToquizUser의 panels 배열에 {panelId, questions, likes} 삽입)

--- server/apps/api/src/panels/panels.repository.ts
+++ server/apps/api/src/panels/panels.repository.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { MysqlPrismaService } from 'libs/prisma/src/mysql-prisma.service';
-import { Panel } from 'shared';
-import { Prisma } from 'libs/prisma/prisma/generated/mysql';
+import { MongodbPrismaService } from 'libs/prisma/src/mongodb-prisma.service';
+import { Panel, ToquizUser } from 'shared';
 import { CreatePanelDto } from '@api/src/panels/dto';
 
 @Injectable()
 export class PanelsRepository {
-  constructor(private mysqlService: MysqlPrismaService) {}
+  constructor(
+    private mysqlService: MysqlPrismaService,
+    private mongodbService: MongodbPrismaService,
+  ) {}
 
   async createPanel(createPanelDto: CreatePanelDto): Promise<Panel> {
     return await this.mysqlService.panel.create({ data: createPanelDto });
   }
 
-  async findPanel(panelWhereInput: Prisma.PanelWhereInput): Promise<Panel> {
-    return await this.mysqlService.panel.findFirst({ where: { ...panelWhereInput } });
+  async getPanelByPanelId(panelId: Panel['id']): Promise<Panel> {
+    return await this.mysqlService.panel.findFirst({ where: { id: panelId } });
+  }
+
+  async getPanelsIdOfToquizUser(
+    toquizUserId: ToquizUser['id'],
+  ): Promise<{ panels: { panelId: string }[] }> {
+    return await this.mongodbService.toquizUser.findUnique({
+      where: { id: toquizUserId },
+      select: { panels: { select: { panelId: true } } },
+    });
+  }
+
+  async createPanelOfToquizUser(toquizUserId, panelId): Promise<void> {
+    await this.mongodbService.toquizUser.update({
+      where: { id: toquizUserId },
+      data: {
+        panels: {
+          push: [{ panelId: panelId, questions: [], likes: [] }],
+        },
+      },
+    });
   }
 }
