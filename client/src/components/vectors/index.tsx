212835813cb8d8efb237e71d9405496dd33694ac
leegwae <leegwae@gmail.com>
2023-04-06 01:29:16 +0900

    chore: Search, Account SVG 컴포넌트를 추가한다 (#165)

--- client/src/components/vectors/index.tsx
+++ client/src/components/vectors/index.tsx
@@ -1,1 +1,4 @@
 export { default as Emergency } from './Emergency';
+export { default as Logo } from './Logo';
+export { default as Search } from './Search';
+export { default as Account } from './Account';
