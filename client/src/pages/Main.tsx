e9d44cce51676de6be583a81593a0b3762aea304
leegwae <leegwae@gmail.com>
2023-04-28 00:27:51 +0900

    design: 메인 페이지 헤딩, 사용자 이름 CSS를 적용한다 (#173)

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -31,11 +31,19 @@ const Main = (): JSX.Element => {
     <>
       <HomeHeader />
       <main>
-        <div>
-          <h1>내 패널 모아보기</h1>
-          <span>{user?.nickname}</span>
+        <div className="mx-auto max-w-7xl">
+          <div className="flex flex-col gap-2 px-3 py-6">
+            <div className="text-off-white">
+              <span className="bg-primary text-off-white rounded-2xl py-1 px-3 text-base font-medium">
+                @{user?.nickname}
+              </span>
+            </div>
+            <h1 className="font text-primary-dark text-4xl font-medium tracking-tight md:text-5xl">
+              내 패널 모아보기
+            </h1>
+          </div>
+          <PanelList panels={panels} />
         </div>
-        <PanelList panels={panels} />
       </main>
     </>
   );
