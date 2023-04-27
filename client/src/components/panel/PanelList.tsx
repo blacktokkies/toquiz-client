c6ffed30264e8e0a7e8156eb45bd8d64c779f679
leegwae <leegwae@gmail.com>
2023-04-28 02:32:32 +0900

    design: `PanelList`에 CSS를 적용한다 (#145)

--- client/src/components/panel/PanelList.tsx
+++ client/src/components/panel/PanelList.tsx
@@ -7,11 +7,24 @@ interface PanelListProps {
 }
 
 const List = ({ panels }: PanelListProps): JSX.Element => (
-  <ul>
+  <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
     {panels.map((panel) => (
-      <li key={panel.id}>
-        <span>{panel.title}</span>
-        <span>{panel.createdAt.toString()}</span>
+      <li
+        key={panel.id}
+        className="shadow-item hover:shadow-item-hover rounded-md bg-white py-4 px-5"
+      >
+        <div className="flex flex-col gap-1 tracking-tight">
+          <div className="text-primary-dark truncate text-base font-bold">
+            {panel.title}
+          </div>
+          <span className="text-grey-dark text-sm">
+            {new Date(panel.createdAt).toLocaleString('ko-KR', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}
+          </span>
+        </div>
       </li>
     ))}
   </ul>
