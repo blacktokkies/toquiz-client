fc1e0232350716c0e1dc30e3d4a8e8780b3e9908
leegwae <leegwae@gmail.com>
2023-05-01 17:19:53 +0900

    refactor: 패널 리스트 mock 데이터에서 생성 함수를 분리한다

--- client/src/mocks/data/panel/myPanelsData.ts
+++ client/src/mocks/data/panel/myPanelsData.ts
@@ -2,18 +2,20 @@ import type { Panel } from 'shared';
 
 import { faker } from '@faker-js/faker';
 
-export const createPanelList = (size = 20): Panel[] =>
-  new Array(size).fill(undefined).map((_, idx) => ({
-    id: `${idx}`,
-    userId: 'userId',
-    title: faker.music.songName(),
-    description: `${idx} 설명`,
-    isArchived: faker.datatype.boolean(),
-    scrapNum: faker.datatype.number({ min: 0, max: 100 }),
-    createdAt: new Date(),
-    deletedAt: null,
-    updatedAt: new Date(),
-  }));
+export const createMockPanel = (): Panel => ({
+  id: faker.datatype.uuid(),
+  userId: 'userId',
+  title: faker.music.songName(),
+  description: faker.lorem.sentence(),
+  isArchived: faker.datatype.boolean(),
+  scrapNum: faker.datatype.number({ min: 0, max: 100 }),
+  createdAt: new Date(),
+  deletedAt: null,
+  updatedAt: new Date(),
+});
+
+export const createMockPanleList = (size: number): Panel[] =>
+  [...Array(size).keys()].map(createMockPanel);
 
 // TODO: myPanelsData로 이름 수정하기
-export const myPanelsData: Panel[] = createPanelList(20 * 3);
+export const myPanelsData: Panel[] = createMockPanleList(60);
