806198b0ecffc97618a5bf522ba9bffae2581fbe
leegwae <leegwae@gmail.com>
2023-05-01 22:41:37 +0900

    refactor: `PanelLIst`에서 `PanelItem`을 분리한다 (#193)

---
+++ client/src/components/panel/PanelItem.tsx
@@ -0,0 +1,29 @@
+import type { Panel } from 'shared';
+
+import React from 'react';
+
+interface PanelItemProps {
+  panel: Panel;
+}
+
+const PanelItem = ({ panel }: PanelItemProps): JSX.Element => (
+  <li
+    key={panel.id}
+    className="shadow-item hover:shadow-item-hover rounded-md bg-white py-4 px-5"
+  >
+    <div className="flex flex-col gap-1 tracking-tight">
+      <div className="text-primary-dark truncate text-base font-bold">
+        {panel.title}
+      </div>
+      <span className="text-grey-dark text-sm">
+        {new Date(panel.createdAt).toLocaleString('ko-KR', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })}
+      </span>
+    </div>
+  </li>
+);
+
+export default PanelItem;
