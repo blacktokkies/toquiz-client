0db7f1ef869af9eb8a7a2521d452513c32b05cb4
leegwae <leegwae@gmail.com>
2023-07-28 15:41:36 +0900

    setting: `server` 패키지를 삭제한다 (#201)

--- server/libs/common/decorators/toquiz-user-id.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const ToquizUserId = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest();
-  return req?.cookies?.toquizToken;
-});
