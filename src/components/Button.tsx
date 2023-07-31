daa1c3a7092d0d4c676bd9f7cce07eb566898d7f
leegwae <leegwae@gmail.com>
2023-07-31 17:39:15 +0900

    setting: `off-black`을 `grey` 색상 상수 값으로 추가한다 (#219)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ const Button = forwardRef<HTMLButtonElement, Props>(
   ({ variant = 'filled', className = '', ...rest }, ref) => (
     <button
       ref={ref}
-      className={`text-off-black hover:bg-primary-hover min-h-[48px] min-w-[128px] rounded-md py-3 font-bold disabled:opacity-30 ${variantStyle[variant]} ${className}`}
+      className={`text-grey-darkest hover:bg-primary-hover min-h-[48px] min-w-[128px] rounded-md py-3 font-bold disabled:opacity-30 ${variantStyle[variant]} ${className}`}
       type="button"
       {...rest}
     />
