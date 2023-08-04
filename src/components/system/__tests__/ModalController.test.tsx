0d5b3bdfd600112de296f7e737215581fc3cd704
leegwae <leegwae@gmail.com>
2023-08-04 18:51:54 +0900

    refactor(use-overlay): `OverlayController`를 `ModalController`로 변경한다 (#241)

--- src/components/system/__tests__/ModalController.test.tsx
+++ src/components/system/__tests__/ModalController.test.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { OverlayController } from '@/components/system/OverlayController';
+import { ModalController } from '@/components/system/ModalController';
 
-describe('OverlayController', () => {
+describe('ModalController', () => {
   const handleClose = vi.fn();
 
   it('오버레이 안쪽을 누르면 close 함수가 호출되지 않는다', async () => {
     render(
-      <OverlayController
-        createOverlayContent={() => <div>오버레이</div>}
+      <ModalController
+        createModalContent={() => <div>오버레이</div>}
         close={handleClose}
       />,
     );
@@ -24,8 +24,8 @@ describe('OverlayController', () => {
 
   it('오버레이 바깥을 누르면 close 함수가 호출된다', async () => {
     render(
-      <OverlayController
-        createOverlayContent={() => <>오버레이</>}
+      <ModalController
+        createModalContent={() => <>오버레이</>}
         close={handleClose}
       />,
     );
