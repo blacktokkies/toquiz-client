de05af14c5187dcd00a4b3dea9978fd3bd08ad68
leegwae <leegwae@gmail.com>
2023-08-04 18:52:32 +0900

    feat(use-overlay): 오버레이 컨텐츠에서 모달 동작을 제거한다 (#241)

--- src/hooks/__tests__/useOverlay.test.tsx
+++ src/hooks/__tests__/useOverlay.test.tsx
@@ -29,16 +29,4 @@ describe('useOverlay', () => {
 
     expect(screen.getByText(/오버레이/)).toBeInTheDocument();
   });
-
-  it('오버레이 바깥을 누르면 오버레이가 언마운트된다', async () => {
-    renderWithOverlay(<TestComponent />);
-
-    const openButton = screen.getByRole('button', { name: /open/ });
-    await userEvent.click(openButton);
-
-    const overlay = screen.getByText(/오버레이/);
-    await userEvent.click(document.body);
-
-    expect(overlay).not.toBeInTheDocument();
-  });
 });
