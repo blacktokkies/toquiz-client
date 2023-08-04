342379bf0518b5e84c3758b6f577aa5528f48a8a
leegwae <leegwae@gmail.com>
2023-08-04 11:20:49 +0900

    design: 바뀐 패널 목록 레이아웃 적용한다 (#234)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home = (): JSX.Element => {
             className="hover:bg-grey-light rounded-full p-2"
           >
             <div role="img" aria-label="내 계정 아이콘">
-              <Account className="fill-grey-darkest" />
+              <Account />
             </div>
           </button>
         </div>
@@ -45,23 +45,36 @@ const Home = (): JSX.Element => {
       <div className="flex-1">
         <div className="container flex h-full max-w-7xl flex-col">
           <div className="flex flex-col items-start gap-5 px-5 py-7">
-            <h1 className="font text-grey-darkest text-2xl font-medium tracking-tighter md:text-5xl">
+            <h1 className="font text-2xl font-medium tracking-tighter md:text-5xl">
               내 패널 모아보기
             </h1>
             <span className="flex gap-1 items-center py-1">
               <div role="img" aria-label="내 계정 아이콘">
-                <Account className="fill-grey-darkest w-5 h-5" />
+                <Account className="w-5 h-5" />
               </div>
-              <span className="text-sm tracking-tight">{user?.nickname}</span>
+              <span className="text-sm">{user?.nickname}</span>
             </span>
           </div>
-          <div className="flex flex-1 flex-col pt-9">
-            <div className="flex justify-between p-3">
-              <span className="text-grey-dark text-sm font-bold">최신순</span>
-              <span className="text-grey-dark text-sm">내 패널</span>
+          <div className="flex flex-1 flex-col">
+            <div
+              role="tablist"
+              className="flex justify-start items-center px-5 py-2 gap-3 border-b border-grey-light"
+            >
+              <div
+                role="tab"
+                className="font-medium rounded-md bg-grey-lighter p-1"
+              >
+                내가 만든 패널
+              </div>
             </div>
-            <div className="bg-off-white flex-1 px-3 pt-4 pb-16">
-              <InfinitePanelGrid />
+            <div role="tabpanel" className="flex flex-1 flex-col">
+              <div className="flex justify-between px-5 py-2 mt-2">
+                <span>최신순</span>
+                <span>총 3개</span>
+              </div>
+              <div className="flex-1 p-5 pb-16 bg-off-white">
+                <InfinitePanelGrid />
+              </div>
             </div>
           </div>
         </div>
