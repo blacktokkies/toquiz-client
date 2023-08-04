fa431d336de68cbd28522a5173716d038ae4ecd9
leegwae <leegwae@gmail.com>
2023-08-04 19:01:48 +0900

    test(modal-controller): 텍스트 컨텐츠를 모달로 변경한다 (#241)

--- src/components/system/__tests__/ModalController.test.tsx
+++ src/components/system/__tests__/ModalController.test.tsx
@@ -8,24 +8,24 @@ import { ModalController } from '@/components/system/ModalController';
 describe('ModalController', () => {
   const handleClose = vi.fn();
 
-  it('오버레이 안쪽을 누르면 close 함수가 호출되지 않는다', async () => {
+  it('모달 안쪽을 누르면 close 함수가 호출되지 않는다', async () => {
     render(
       <ModalController
-        createModalContent={() => <div>오버레이</div>}
+        createModalContent={() => <div>모달</div>}
         close={handleClose}
       />,
     );
 
-    const overlay = screen.getByText(/오버레이/);
+    const overlay = screen.getByText(/모달/);
     await userEvent.click(overlay);
 
     expect(handleClose).not.toHaveBeenCalled();
   });
 
-  it('오버레이 바깥을 누르면 close 함수가 호출된다', async () => {
+  it('모달 바깥을 누르면 close 함수가 호출된다', async () => {
     render(
       <ModalController
-        createModalContent={() => <>오버레이</>}
+        createModalContent={() => <>모달</>}
         close={handleClose}
       />,
     );
