681c0d31b4714a92e7c02c3acb98261a0e08aede
leegwae <leegwae@gmail.com>
2023-08-04 12:26:03 +0900

    feat(intersection-area): 요소가 교차될 때마다 콜백을 실행하는 컴포넌트를 작성한다 (#238)

---
+++ src/components/system/IntersectionArea.tsx
@@ -0,0 +1,29 @@
+import type { PropsWithChildren } from 'react';
+
+import React, { useEffect, useRef } from 'react';
+
+interface Props {
+  onIntersection: (isIntersecting: boolean) => void;
+}
+
+export function IntersectionArea({
+  children,
+  onIntersection,
+}: Props & PropsWithChildren): JSX.Element {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (targetRef.current === null) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      onIntersection(entry.isIntersecting);
+    });
+
+    observer.observe(targetRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [onIntersection]);
+
+  return <div ref={targetRef}>{children}</div>;
+}
