404bc89e28ce91f60b75646689735ac4a128f59d
leegwae <leegwae@gmail.com>
2023-08-07 13:10:43 +0900

    refactor(input): props 순서나 className 순서를 정리한다

--- src/components/system/Input.tsx
+++ src/components/system/Input.tsx
@@ -2,22 +2,31 @@ import type { InputHTMLAttributes } from 'react';
 
 import React, { forwardRef } from 'react';
 
+import { clsx } from 'clsx';
+
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string | null;
 }
 
-const Input = forwardRef<HTMLInputElement, Props>(
-  ({ errorMessage, ...rest }: Props, ref) => (
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { errorMessage, ...rest }: Props,
+  ref,
+) {
+  return (
     <>
       <input
-        className="text-md text-grey-darkest border-grey-light focus:border-primary hover:border-primary placeholder:text-grey bg-off-white disabled:bg-grey-light disabled:hover:border-grey-light rounded-md border py-3 px-4 outline-none focus:bg-white disabled:pointer-events-none"
+        className={clsx(
+          'py-3 px-4 border border-grey-light outline-none bg-off-white',
+          'text-md text-grey-darkest placeholder:text-grey',
+          'hover:border-primary focus:border-primary focus:bg-white',
+          'disabled:bg-grey-light disabled:hover:border-grey-light disabled:pointer-events-none',
+        )}
         ref={ref}
         {...rest}
       />
       {errorMessage && <div className="text-danger px-1">{errorMessage}</div>}
     </>
-  ),
-);
+  );
+});
 
-Input.displayName = 'Input';
-export default Input;
+export default React.memo(Input);
