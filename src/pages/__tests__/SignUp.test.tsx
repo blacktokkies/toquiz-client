99c0092600e640b60a030d844f2acbb715f97b70
leegwae <leegwae@gmail.com>
2023-08-08 18:08:52 +0900

    feat(sign-up): 회원가입 시 이메일이 중복되면 에러 메시지를 보여준다 (#263)

---
+++ src/pages/__tests__/SignUp.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { MemoryRouter } from 'react-router-dom';
+
+import { apiUrl } from '@/lib/apiUrl';
+import { renderWithQueryClient } from '@/lib/test-utils';
+import { server } from '@/mocks/server';
+import Signup from '@/pages/SignUp';
+
+function overrideSignUpResultWithError(): void {
+  server.use(
+    rest.post(apiUrl.auth.signup(), async (req, res, ctx) =>
+      res(
+        ctx.status(400),
+        ctx.json({
+          code: 'DUPLICATE_EMAIL',
+          statusCode: 400,
+          message: '이미 존재하는 email 입니다.',
+        }),
+      ),
+    ),
+  );
+}
+
+describe('회원가입 페이지', () => {
+  it('중복된 이메일을 제출하면 에러 메시지를 보여준다', async () => {
+    vi.mock('@/lib/validator', () => ({
+      isEmail: vi.fn().mockImplementation(() => true),
+      isNickname: vi.fn().mockImplementation(() => true),
+      isPassword: vi.fn().mockImplementation(() => true),
+    }));
+    overrideSignUpResultWithError();
+
+    renderWithQueryClient(
+      <MemoryRouter>
+        <Signup />
+      </MemoryRouter>,
+    );
+
+    const submitButton = screen.getByRole('button', { name: '회원가입' });
+    await userEvent.click(submitButton);
+
+    expect(screen.getByText(/이미 존재하는 계정입니다/)).toBeInTheDocument();
+  });
+});
