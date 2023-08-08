f247ee374249de375ca457d10426ddcc9b738f2c
leegwae <leegwae@gmail.com>
2023-08-09 02:23:25 +0900

    test(signup-page): 회원가입 성공시 로그인 페이지로 이동하는지 테스트한다 (#274)
    * test(signup-page): 제출된 필드가 모두 유효하면 로그인 페이지로 이동하는지 테스트한다 (#33)
    
    * test(signup-page): 리팩토링 (#33)

--- src/pages/__tests__/SignUp.test.tsx
+++ src/pages/__tests__/SignUp.test.tsx
@@ -2,30 +2,67 @@ import type { ErrorResponse } from '@/lib/api/response';
 
 import React from 'react';
 
-import { screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 
+import * as apis from '@/lib/api/auth';
 import { apiUrl } from '@/lib/apiUrl';
 import { renderWithQueryClient } from '@/lib/test-utils';
 import { server } from '@/mocks/server';
 import { SignUp } from '@/pages/SignUp';
 
-function overrideSignUpResultWithError(data: ErrorResponse): void {
-  server.use(
-    rest.post(apiUrl.auth.signup(), async (req, res, ctx) =>
-      res(ctx.status(data.statusCode), ctx.json(data)),
-    ),
-  );
-}
+vi.mock('@/lib/validator', () => ({
+  isEmail: vi.fn(() => true),
+  isNickname: vi.fn(() => true),
+  isPassword: vi.fn(() => true),
+}));
+
+const navigateMockFn = vi.fn();
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const router = (await importOriginal()) ?? {};
+  return { ...router, useNavigate: vi.fn(() => navigateMockFn) };
+});
 
 describe('회원가입 페이지', () => {
-  vi.mock('@/lib/validator', () => ({
-    isEmail: vi.fn().mockImplementation(() => true),
-    isNickname: vi.fn().mockImplementation(() => true),
-    isPassword: vi.fn().mockImplementation(() => true),
-  }));
+  afterEach(() => vi.restoreAllMocks());
+
+  it('유효한 형식의 이메일, 비밀번호, 닉네임을 제출하면 회원가입 API를 호출하고 로그인 페이지로 이동한다', async () => {
+    const spyOnSignUp = vi.spyOn(apis, 'signUp');
+
+    const {
+      submitButton,
+      emailInput,
+      passwordInput,
+      confirmPasswordInput,
+      nicknameInput,
+    } = setup();
+
+    fireEvent.change(emailInput, { target: { value: '유효한 이메일' } });
+    fireEvent.change(passwordInput, {
+      target: { value: '유효한 비밀번호' },
+    });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: '유효한 비밀번호' },
+    });
+    fireEvent.change(nicknameInput, {
+      target: { value: '유효한 닉네임' },
+    });
+
+    await userEvent.click(submitButton);
+
+    expect(spyOnSignUp).toHaveBeenCalledWith({
+      email: '유효한 이메일',
+      password: '유효한 비밀번호',
+      nickname: '유효한 닉네임',
+    });
+
+    await waitFor(() => {
+      expect(navigateMockFn).toHaveBeenCalledWith('/login');
+    });
+  });
 
   it('중복된 이메일을 제출하면 에러 메시지를 보여준다', async () => {
     overrideSignUpResultWithError({
@@ -34,13 +71,7 @@ describe('회원가입 페이지', () => {
       message: '이미 존재하는 email 입니다.',
     });
 
-    renderWithQueryClient(
-      <MemoryRouter>
-        <SignUp />
-      </MemoryRouter>,
-    );
-
-    const submitButton = screen.getByRole('button', { name: '회원가입' });
+    const { submitButton } = setup();
     await userEvent.click(submitButton);
 
     await waitFor(() => {
@@ -49,6 +80,7 @@ describe('회원가입 페이지', () => {
       ).toBeInTheDocument();
     });
   });
+
   it('중복된 닉네임을 제출하면 에러 메시지를 보여준다', async () => {
     overrideSignUpResultWithError({
       code: 'DUPLICATE_NICKNAME',
@@ -56,13 +88,7 @@ describe('회원가입 페이지', () => {
       message: '이미 존재하는 nickname 입니다.',
     });
 
-    renderWithQueryClient(
-      <MemoryRouter>
-        <SignUp />
-      </MemoryRouter>,
-    );
-
-    const submitButton = screen.getByRole('button', { name: '회원가입' });
+    const { submitButton } = setup();
     await userEvent.click(submitButton);
 
     await waitFor(() => {
@@ -72,7 +98,7 @@ describe('회원가입 페이지', () => {
     });
   });
 
-  it('유효하지 않은 이메일, 닉네임, 비밀번호를 제출하면 에러 메시지를 보여준다.', async () => {
+  it('유효하지 않은 이메일, 비밀번호, 닉네임을 제출하면 에러 메시지를 보여준다.', async () => {
     overrideSignUpResultWithError({
       code: 'INVALID_PARAMETER',
       statusCode: 400,
@@ -94,13 +120,8 @@ describe('회원가입 페이지', () => {
       ],
     });
 
-    renderWithQueryClient(
-      <MemoryRouter>
-        <SignUp />
-      </MemoryRouter>,
-    );
+    const { submitButton } = setup();
 
-    const submitButton = screen.getByRole('button', { name: '회원가입' });
     await userEvent.click(submitButton);
 
     await waitFor(() => {
@@ -123,3 +144,40 @@ describe('회원가입 페이지', () => {
     });
   });
 });
+
+function overrideSignUpResultWithError(data: ErrorResponse): void {
+  server.use(
+    rest.post(apiUrl.auth.signup(), async (req, res, ctx) =>
+      res(ctx.status(data.statusCode), ctx.json(data)),
+    ),
+  );
+}
+
+function setup(): {
+  emailInput: HTMLElement;
+  passwordInput: HTMLElement;
+  confirmPasswordInput: HTMLElement;
+  nicknameInput: HTMLElement;
+  submitButton: HTMLElement;
+} {
+  renderWithQueryClient(
+    <MemoryRouter>
+      <SignUp />
+    </MemoryRouter>,
+  );
+
+  const emailInput = screen.getByLabelText('이메일 인풋');
+  const passwordInput = screen.getByLabelText('비밀번호 인풋');
+  const confirmPasswordInput = screen.getByLabelText('비밀번호 확인 인풋');
+  const nicknameInput = screen.getByLabelText('닉네임 인풋');
+
+  const submitButton = screen.getByRole('button', { name: '회원가입' });
+
+  return {
+    emailInput,
+    passwordInput,
+    confirmPasswordInput,
+    nicknameInput,
+    submitButton,
+  };
+}
