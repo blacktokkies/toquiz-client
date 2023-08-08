609f448da673a34c8227f26bf60f74360752b365
leegwae <leegwae@gmail.com>
2023-08-08 13:23:18 +0900

    fix(api-auth): 엔드포인트 변경을 반영한다 (#260)

--- src/lib/apiUrl.ts
+++ src/lib/apiUrl.ts
@@ -1,15 +1,18 @@
-const authApiUrl = {
-  signup: () => `/api/users/auth/signup` as const,
-  login: () => `/api/users/auth/login` as const,
-  me: () => `/api/user` as const,
-  refresh: () => `/api/users/auth/refresh` as const,
-};
+const auth = {
+  signup: () => `/api/auth/signup`,
+  login: () => `/api/auth/login`,
+  logout: () => `/api/auth/logout`,
+  signout: () => `/api/auth/signout`,
+  update: () => `/api/auth/update`,
+  refresh: () => `/api/auth/refresh`,
+  me: () => `/api/auth/me`,
+} as const;
 
 const panelApiUrl = {
   getMyPanels: () => `/api/panels` as const,
 };
 
 export const apiUrl = {
-  auth: authApiUrl,
+  auth,
   panel: panelApiUrl,
 };
