f16b5fc014dd3f74861b63c81323fcf6bd4f2e5b
leegwae <leegwae@gmail.com>
2023-08-09 02:37:33 +0900

    test(login-page): 로그인 성공 시 홈 페이지로 이동하는지 테스트한다 (#34)

--- src/pages/__tests__/Login.test.tsx
+++ src/pages/__tests__/Login.test.tsx
@@ -9,30 +9,57 @@ import { renderWithQueryClient } from '@/lib/test-utils';
 import { Login } from '@/pages/Login';
 import { getUser } from '@/store/userStore';
 
+const navigateMockFn = vi.fn();
+
 describe('로그인 페이지', () => {
+  afterEach(() => vi.restoreAllMocks());
+
   vi.mock('@/lib/validator', () => ({
-    isEmail: vi.fn().mockImplementation(() => true),
-    isPassword: vi.fn().mockImplementation(() => true),
+    isEmail: vi.fn(() => true),
+    isPassword: vi.fn(() => true),
   }));
 
-  it('유효한 형식의 이메일, 비밀번호를 제출하면 로그인 API를 호출하고 스토어에 저장한다', async () => {
+  vi.mock('react-router-dom', async (importOriginal) => {
+    const router = (await importOriginal()) ?? {};
+    return { ...router, useNavigate: vi.fn(() => navigateMockFn) };
+  });
+
+  it('유효한 형식의 이메일, 비밀번호를 제출하면 로그인 API를 호출하고 스토어에 저장한 후 홈 페이지로 이동한다', async () => {
     const spyOnLogin = vi.spyOn(apis, 'login');
 
-    renderWithQueryClient(
-      <MemoryRouter>
-        <Login />
-      </MemoryRouter>,
-    );
-
-    const emailInput = screen.getByLabelText('이메일 인풋');
+    const { emailInput, passwordInput, submitButton } = setup();
     fireEvent.change(emailInput, { target: { value: '유효한 이메일' } });
-
-    const submitButton = screen.getByRole('button', { name: '로그인' });
+    fireEvent.change(passwordInput, { target: { value: '유효한 비밀번호' } });
     await userEvent.click(submitButton);
 
-    expect(spyOnLogin).toHaveBeenCalled();
+    expect(spyOnLogin).toHaveBeenCalledWith({
+      email: '유효한 이메일',
+      password: '유효한 비밀번호',
+    });
     await waitFor(() => {
       expect(getUser()?.email).toBe('유효한 이메일');
     });
+    await waitFor(() => {
+      expect(navigateMockFn).toHaveBeenCalledWith('/home');
+    });
   });
 });
+
+function setup(): {
+  emailInput: HTMLElement;
+  passwordInput: HTMLElement;
+  submitButton: HTMLElement;
+} {
+  renderWithQueryClient(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>,
+  );
+
+  const emailInput = screen.getByLabelText('이메일 인풋');
+  const passwordInput = screen.getByLabelText('비밀번호 인풋');
+
+  const submitButton = screen.getByRole('button', { name: '로그인' });
+
+  return { emailInput, passwordInput, submitButton };
+}
