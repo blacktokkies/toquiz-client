7e46a034662c5165b28ee182d2649a977a75f90c
leegwae <leegwae@gmail.com>
2023-08-08 13:33:14 +0900

    style: 정의 순서를 변경한다

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -3,6 +3,26 @@ import type { SuccessResponse } from '@/lib/api/response';
 import { apiClient } from '@/lib/apiClient';
 import { apiUrl } from '@/lib/apiUrl';
 
+export const signUp = async (body: SignUpBody): Promise<SignUpResult> =>
+  apiClient
+    .post<SignUpResponse, SignUpBody>(apiUrl.auth.signup(), body)
+    .then((data) => data.result);
+
+export const login = async (body: LogInBody): Promise<LogInResult> =>
+  apiClient
+    .post<LogInResponse, LogInBody>(apiUrl.auth.login(), body)
+    .then((data) => data.result);
+
+export const me = async (): Promise<GetMyInfoResult> =>
+  apiClient
+    .get<GetMyInfoResponse>(apiUrl.auth.me())
+    .then((data) => data.result);
+
+export const refresh = async (): Promise<RefreshResult> =>
+  apiClient
+    .post<RefreshResponse>(apiUrl.auth.refresh())
+    .then((data) => data.result);
+
 export interface SignUpBody {
   email: Member['email'];
   password: Member['password'];
@@ -14,11 +34,6 @@ export interface SignUpResult {
 }
 
 export type SignUpResponse = SuccessResponse<SignUpResult>;
-
-export const signUp = async (body: SignUpBody): Promise<SignUpResult> =>
-  apiClient
-    .post<SignUpResponse, SignUpBody>(apiUrl.auth.signup(), body)
-    .then((data) => data.result);
 
 export interface LogInBody {
   email: Member['email'];
@@ -37,11 +52,6 @@ export interface LogInResult {
 
 export type LogInResponse = SuccessResponse<LogInResult>;
 
-export const login = async (body: LogInBody): Promise<LogInResult> =>
-  apiClient
-    .post<LogInResponse, LogInBody>(apiUrl.auth.login(), body)
-    .then((data) => data.result);
-
 export interface GetMyInfoResult {
   id: Member['id'];
   email: Member['email'];
@@ -54,11 +64,6 @@ export interface GetMyInfoResult {
 
 export type GetMyInfoResponse = SuccessResponse<GetMyInfoResult>;
 
-export const me = async (): Promise<GetMyInfoResult> =>
-  apiClient
-    .get<GetMyInfoResponse>(apiUrl.auth.me())
-    .then((data) => data.result);
-
 export interface RefreshResult {
   user: {
     id: Member['id'];
@@ -70,11 +75,6 @@ export interface RefreshResult {
 }
 
 export type RefreshResponse = SuccessResponse<RefreshResult>;
-
-export const refresh = async (): Promise<RefreshResult> =>
-  apiClient
-    .post<RefreshResponse>(apiUrl.auth.refresh())
-    .then((data) => data.result);
 
 export interface Member {
   id: string;
