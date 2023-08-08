0d23b1ecb8ebc9c0340a5514d7d7c627dfdfa9d4
leegwae <leegwae@gmail.com>
2023-08-08 21:54:34 +0900

    feat(login-page): 유효한 필드를 제출하면 로그인 API를 호출한다 (#269)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,22 @@
-import type { LogInBody } from '@/lib/api/auth';
 import type { LoaderFunction } from 'react-router-dom';
 
 import React from 'react';
 
 import { useSearchParams, useNavigate, Link, redirect } from 'react-router-dom';
 
-import LoginForm from '@/components/auth/LoginForm';
+import Button from '@/components/system/Button';
+import { LabelInput } from '@/components/system/LabelInput';
 import { useLoginMutation } from '@/hooks/queries/auth';
 import { useUserStore } from '@/hooks/store/useUserStore';
+import { useForm } from '@/hooks/useForm';
 import { setAccessToken } from '@/lib/apiClient';
 import { isUserLoggedIn } from '@/lib/routeGuard';
+import { isEmail, isPassword } from '@/lib/validator';
+
+export const LOGIN_ERROR_MESSAGE = {
+  email: '이메일 형식의 아이디를 입력하세요',
+  password: '8~20자 이하의 영문 대소문자, 숫자, 특수기호를 입력하세요',
+} as const;
 
 export const loginLoader: LoaderFunction = async () => {
   const isLoggedIn = await isUserLoggedIn();
@@ -26,23 +33,67 @@ function Login(): JSX.Element {
   const next = searchParams.get('next') ?? '/home';
 
   const loginMutation = useLoginMutation();
-  const handleSubmit = ({ email, password }: LogInBody): void => {
-    loginMutation.mutate(
-      { email, password },
-      {
-        onSuccess: ({ accessToken, user }) => {
-          setAccessToken(accessToken);
-          setUser(user);
-          navigate(`${next}`);
-        },
+  const { inputProps, errors, formProps, hasError } = useForm({
+    inputConfigs: {
+      email: {
+        validate: (value) => isEmail(value),
+        errorMessage: LOGIN_ERROR_MESSAGE.email,
       },
-    );
-  };
+      password: {
+        validate: (value) => isPassword(value),
+        errorMessage: LOGIN_ERROR_MESSAGE.password,
+      },
+    },
+    formConfig: {
+      onSubmit: ({ email, password }) => {
+        loginMutation.mutate(
+          { email, password },
+          {
+            onSuccess: ({ accessToken, ...rest }) => {
+              setAccessToken(accessToken);
+              setUser(rest);
+              navigate(`${next}`);
+            },
+            onError: (error, body) => {
+              console.log(error);
+            },
+          },
+        );
+      },
+    },
+  });
 
   return (
     <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center px-9">
       <div className="mb-14 text-3xl font-bold">toquiz</div>
-      <LoginForm action={handleSubmit} />
+      <form className="flex w-full flex-col gap-8" {...formProps}>
+        <div className="flex flex-col gap-5">
+          <LabelInput
+            label="이메일"
+            required
+            placeholder="이메일을 입력하세요"
+            errorMessage={errors.email}
+            {...inputProps.email}
+            aria-label="이메일 인풋"
+          />
+          <LabelInput
+            label="비밀번호"
+            type="password"
+            required
+            placeholder="비밀번호를 입력하세요"
+            errorMessage={errors.password}
+            {...inputProps.password}
+            aria-label="비밀번호 인풋"
+          />
+        </div>
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={hasError || loginMutation.isLoading}
+        >
+          로그인
+        </Button>
+      </form>
       <div className="mt-2">
         계정이 없나요?{' '}
         <Link className="font-bold underline" to="/signup">
