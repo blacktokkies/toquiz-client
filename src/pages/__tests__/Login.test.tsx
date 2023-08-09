bbf4416d0be9186a26d5c757138994e649023b6f
leegwae <leegwae@gmail.com>
2023-08-09 11:18:43 +0900

    feat(login-page): 로그인 시 존재하지 않는 계정을 제출하면 에러 메시지를 보여준다 (#267)

--- src/pages/__tests__/Login.test.tsx
+++ src/pages/__tests__/Login.test.tsx
@@ -1,11 +1,16 @@
+import type { ErrorResponse } from '@/lib/api/response';
+
 import React from 'react';
 
 import { fireEvent, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 
 import * as apis from '@/lib/api/auth';
+import { apiUrl } from '@/lib/apiUrl';
 import { renderWithQueryClient } from '@/lib/test-utils';
+import { server } from '@/mocks/server';
 import { Login } from '@/pages/Login';
 import { getUser } from '@/store/userStore';
 
@@ -43,7 +48,41 @@ describe('로그인 페이지', () => {
       expect(navigateMockFn).toHaveBeenCalledWith('/home');
     });
   });
+
+  it('사용자가 제출한 이메일로 된 계정이 존재하지 않으면 에러 메시지를 보여준다', async () => {
+    overrideLoginResultWithError({
+      statusCode: 400,
+      code: 'NON_EXISTENT_ACCOUNT',
+      message: '계정이 존재하지 않습니다',
+    });
+
+    const spyOnLogin = vi.spyOn(apis, 'login');
+
+    const { emailInput, passwordInput, submitButton } = setup();
+    fireEvent.change(emailInput, {
+      target: { value: '존재하지 않는 계정 이메일' },
+    });
+    fireEvent.change(passwordInput, {
+      target: { value: '비밀번호' },
+    });
+    await userEvent.click(submitButton);
+
+    expect(spyOnLogin).toHaveBeenCalledWith({
+      email: '존재하지 않는 계정 이메일',
+      password: '비밀번호',
+    });
+
+    expect(screen.getByText(/존재하지 않는 계정입니다/));
+  });
 });
+
+function overrideLoginResultWithError(data: ErrorResponse): void {
+  server.use(
+    rest.post(apiUrl.auth.login(), async (req, res, ctx) =>
+      res(ctx.status(data.statusCode), ctx.json(data)),
+    ),
+  );
+}
 
 function setup(): {
   emailInput: HTMLElement;
