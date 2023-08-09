bf44f436e3244894b01096941e51376c08ff3a2b
leegwae <leegwae@gmail.com>
2023-08-09 19:03:32 +0900

    test(zustand): `zustsand` 라이브러리를 모킹한다 (#280)
    - 테스트가 끝날 때마다 사용한 스토어를 초기화한다

---
+++ __mocks__/zustand.ts
@@ -0,0 +1,47 @@
+// https://docs.pmnd.rs/zustand/guides/testing#vitest
+import type * as zustand from 'zustand';
+
+import { act } from '@testing-library/react';
+
+const {
+  create: actualCreate,
+  createStore: actualCreateStore,
+  useStore: actualUseStore,
+} = await vi.importActual<typeof zustand>('zustand');
+
+// a variable to hold reset functions for all stores declared in the app
+export const storeResetFns = new Set<() => void>();
+
+// when creating a store, we get its initial state, create a reset function and add it in the set
+export const create = (<T>() =>
+  (stateCreator: zustand.StateCreator<T>) => {
+    const store = actualCreate<T>(stateCreator);
+    const initialState = store.getState();
+    storeResetFns.add(() => {
+      store.setState(initialState, true);
+    });
+    return store;
+  }) as typeof zustand.create;
+
+export const createStore = <T>(
+  initializer: zustand.StateCreator<T>,
+): zustand.StoreApi<T> => {
+  const store = actualCreateStore(initializer);
+  const initialState = store.getState();
+  storeResetFns.add(() => {
+    store.setState(initialState, true);
+  });
+
+  return store;
+};
+
+export const useStore = actualUseStore;
+
+// reset all stores after each test run
+afterEach(() => {
+  act(() => {
+    storeResetFns.forEach((resetFn) => {
+      resetFn();
+    });
+  });
+});
