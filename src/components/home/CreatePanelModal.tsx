857d23f891d02ec758917e6d35817830ce8f11cf
leegwae <leegwae@gmail.com>
2023-08-10 18:51:28 +0900

    feat(create-panel): 유효하지 않은 제목 입력시 에러 메시지 보여준다 (#293)

--- src/components/home/CreatePanelModal.tsx
+++ src/components/home/CreatePanelModal.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-import { LabelInput } from '../system/LabelInput';
+import { LabelInput } from '@/components/system/LabelInput';
+import { useForm } from '@/hooks/useForm';
+import { isPanelTitle } from '@/lib/validator';
 
 interface Props {
   close: () => void;
 }
 export function CreatePanelModal({ close }: Props): JSX.Element {
+  const { inputProps, errors } = useForm({
+    inputConfigs: {
+      title: {
+        validate: (value) => isPanelTitle(value),
+        errorMessage: '패널 제목은 3 ~ 40자이어야 합니다',
+      },
+    },
+  });
+
   return (
     <div>
       <h2>패널 생성하기</h2>
       <form aria-label="패널 생성" onSubmit={close}>
-        <LabelInput label="패널 제목" />
+        <LabelInput
+          label="패널 제목"
+          required
+          placeholder="패널 제목을 입력하세요"
+          aria-label="패널 제목 인풋"
+          errorMessage={errors.title}
+          {...inputProps.title}
+        />
         <LabelInput label="패널 설명" />
         <button
           type="button"
--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -8,3 +8,5 @@ export const isPassword: Validator = (value) =>
   /^(?=\S*[a-zA-z])(?=\S*[0-9])(?=\S*[$`~!@$!%*#^?&\\(\\)\-_=+])\S{8,20}$/.test(
     value,
   );
+export const isPanelTitle: Validator = (value) =>
+  /^.{3,40}$/.test(value.trim());
