439c0f187e8127e75c3b4fee60fc6e111177f3e6
leegwae <leegwae@gmail.com>
2023-08-10 15:26:05 +0900

    refactor(panel-item): 날짜를 포매팅하는 로직을 함수로 분리한다 (#288)

--- src/components/home/PanelItem.tsx
+++ src/components/home/PanelItem.tsx
@@ -6,22 +6,29 @@ interface PanelItemProps {
   panel: Panel;
 }
 
-export const PanelItem = ({ panel }: PanelItemProps): JSX.Element => (
-  <li
-    key={panel.id}
-    className="shadow-item hover:shadow-item-hover rounded-md bg-white py-4 px-5"
-  >
-    <div className="flex flex-col gap-1 tracking-tight">
-      <div className="text-primary-dark truncate text-base font-bold">
-        {panel.title}
+export const PanelItem = ({ panel }: PanelItemProps): JSX.Element => {
+  const { id, title, createdAt } = panel;
+
+  return (
+    <li
+      key={id}
+      className="shadow-item hover:shadow-item-hover rounded-md bg-white py-4 px-5"
+    >
+      <div className="flex flex-col gap-1 tracking-tight">
+        <div className="text-primary-dark truncate text-base font-bold">
+          {title}
+        </div>
+        <span className="text-grey-dark text-sm">
+          {formatDateString(createdAt)}
+        </span>
       </div>
-      <span className="text-grey-dark text-sm">
-        {new Date(panel.createdAt).toLocaleString('ko-KR', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
-      </span>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
+
+export const formatDateString = (date: string): string =>
+  new Date(date).toLocaleString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
