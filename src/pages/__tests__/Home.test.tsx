ec18c3d665793562fbdabe248d56cdb0f38b1d6c
leegwae <leegwae@gmail.com>
2023-08-10 16:46:33 +0900

    test(home-page): 리팩토링

--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -5,18 +5,17 @@ import { MemoryRouter } from 'react-router-dom';
 import { it } from 'vitest';
 
 import { renderWithQueryClient } from '@/lib/test-utils';
+import { myPanelList } from '@/mocks/data/panel';
 import { Home } from '@/pages/Home';
 
 describe('홈 페이지', () => {
-  vi.mock('@/hooks/stores/useUserStore', () => {
-    const useUserStore = vi.fn();
-    useUserStore.mockImplementation(() => ({
+  vi.mock('@/hooks/stores/useUserStore', () => ({
+    useUserStore: vi.fn(() => ({
       nickname: '테스트 닉네임',
-    }));
-    return {
-      useUserStore,
-    };
-  });
+      email: '테스트 이메일',
+      createdAt: new Date().toString(),
+    })),
+  }));
 
   it('홈 페이지 헤더를 보여준다', () => {
     renderWithQueryClient(
@@ -27,6 +26,7 @@ describe('홈 페이지', () => {
 
     expect(screen.getByRole('banner')).toBeInTheDocument();
   });
+
   it('내 패널 모아보기 헤딩을 보여준다.', () => {
     renderWithQueryClient(
       <MemoryRouter>
@@ -55,10 +55,7 @@ describe('홈 페이지', () => {
     );
 
     await waitFor(() => {
-      // TODO: myPanelList 사용하려했는데 Cannot read properties of undefined 오류가 자꾸 뜬다
-      // 현재 Panel mock 데이터는 fakerjs로 생성하여 a가 무조건 포함되는 것을 기대할 수 있으므로
-      // 일단 a가 출력되는지 보도록 한다.
-      expect(screen.getByText(/a/i)).toBeInTheDocument();
+      expect(screen.getByText(myPanelList[0].title)).toBeInTheDocument();
     });
   });
 });
