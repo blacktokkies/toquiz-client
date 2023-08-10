64e581bc2883079da0d2929df70ade58bb947ae5
leegwae <leegwae@gmail.com>
2023-08-10 17:34:51 +0900

    feat(home-page): 패널 생성 버튼을 보여준다 (#291)

--- src/components/home/InfinitePanelGrid.tsx
+++ src/components/home/InfinitePanelGrid.tsx
@@ -1,12 +1,15 @@
 import React, { useCallback } from 'react';
 
+import { PanelGrid } from '@/components/home/PanelGrid';
 import { IntersectionArea } from '@/components/system/IntersectionArea';
 import { useMyPanelsInfiniteQuery } from '@/hooks/queries/panel';
+import { useOverlay } from '@/hooks/useOverlay';
 
-import { PanelGrid } from './PanelGrid';
+import { ModalController } from '../system/ModalController';
 
 export const InfinitePanelGrid = (): JSX.Element => {
   const panelsQuery = useMyPanelsInfiniteQuery();
+  const overlay = useOverlay();
 
   const fetchPanels = useCallback(
     (isIntersecting: boolean) => {
@@ -20,6 +23,14 @@ export const InfinitePanelGrid = (): JSX.Element => {
     [panelsQuery],
   );
 
+  function handleClick(): void {
+    overlay.open(({ close }) => (
+      <ModalController close={close} ariaLabel="패널 생성 모달">
+        <div>패널 생성 모달</div>
+      </ModalController>
+    ));
+  }
+
   if (panelsQuery.isLoading) return <div>loading...</div>;
   if (panelsQuery.isError) return <div>error occurred</div>;
 
@@ -27,6 +38,9 @@ export const InfinitePanelGrid = (): JSX.Element => {
     <div className="flex-1 p-5 bg-off-white">
       <PanelGrid panelPages={panelsQuery.data.pages} />
       <IntersectionArea onIntersection={fetchPanels} />
+      <button type="button" onClick={handleClick}>
+        열기
+      </button>
     </div>
   );
 };
