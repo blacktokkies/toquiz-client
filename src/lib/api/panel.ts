0459dc124a3531df15829acac6c867392e896db7
leegwae <leegwae@gmail.com>
2023-08-10 22:13:50 +0900

    feat(panel-api): 패널 생성 API 호출 함수를 작성한다 (#293)

--- src/lib/api/panel.ts
+++ src/lib/api/panel.ts
@@ -1,3 +1,4 @@
+import type { SuccessResponse } from './response';
 import type {
   GetMyPanelsParams,
   GetMyPanelsResponse,
@@ -17,11 +18,36 @@ export const getMyPanels = async (
     )
     .then((data) => data.result);
 
+export const createPanel = async (
+  body: CreatePanelBody,
+): Promise<CreatePanelResult> =>
+  apiClient
+    .post<CreatePanelResponse, CreatePanelBody>(apiUrl.panel.create(), body)
+    .then((data) => data.result);
+
+/* ================================ [패널 생성 API] ====================================== */
+export interface CreatePanelBody {
+  title: string;
+  description?: string;
+}
+
+export interface CreatePanelResult {
+  id: Panel['id'];
+  userId: Panel['userId'];
+  title: Panel['title'];
+  description: Panel['description'];
+  createdAt: Panel['createdAt'];
+}
+
+export type CreatePanelResponse = SuccessResponse<CreatePanelResult>;
+
+/* ================================ [패널 엔티티] ====================================== */
+
 export interface Panel {
   id: string;
   userId: string;
   title: string;
-  description: string;
+  description?: string;
   createdAt: string;
   updatedAt: string;
   deletedAt?: string;
