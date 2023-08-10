7cb8675774f76d5bc8d98c207e2ca449509a1fee
leegwae <leegwae@gmail.com>
2023-08-10 16:37:53 +0900

    design(panel-grid): 패널 목록 없을 때 안내 문구 디자인 적용한다 (#288)

--- src/components/home/PanelGrid.tsx
+++ src/components/home/PanelGrid.tsx
@@ -4,12 +4,25 @@ import React from 'react';
 
 import { PanelItem } from '@/components/home/PanelItem';
 
+import { Logo } from '../vectors';
+
 interface Props {
   panelPages: GetMyPanelsResult[];
 }
 
 export function PanelGrid({ panelPages }: Props): JSX.Element {
-  if (panelPages.length === 0) return <div>아직 작성한 패널이 없습니다</div>;
+  if (panelPages.length === 0)
+    return (
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="flex items-center gap-1 font-bold">
+          <div className="text-lg">아직 작성한 패널이 없습니다</div>
+          <Logo role="img" aria-label="로고" width="36" height="36" />
+        </div>
+        <div className="text-grey-dark">
+          새로운 패널을 만들어 질문을 받아보세요!
+        </div>
+      </div>
+    );
 
   return (
     <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 pb-16">
