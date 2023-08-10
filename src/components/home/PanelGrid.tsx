8e27a4bf369161af6618a39856bddb06c1a2d6c5
leegwae <leegwae@gmail.com>
2023-08-10 16:19:59 +0900

    feat(panel-grid): 아직 작성한 패널이 없으면 안내 문구를 보여준다 (#288)

--- src/components/home/PanelGrid.tsx
+++ src/components/home/PanelGrid.tsx
@@ -9,6 +9,8 @@ interface Props {
 }
 
 export function PanelGrid({ panelPages }: Props): JSX.Element {
+  if (panelPages.length === 0) return <div>아직 작성한 패널이 없습니다</div>;
+
   return (
     <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3 pb-16">
       {panelPages.map((page) =>
