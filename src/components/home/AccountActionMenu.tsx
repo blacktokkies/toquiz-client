95fcba5924ee5decd9b4f643c253076ea01b49d0
leegwae <leegwae@gmail.com>
2023-08-11 02:34:32 +0900

    design(home-page): 계정 액션 메뉴 디자인 적용한다 (#302)

--- src/components/home/AccountActionMenu.tsx
+++ src/components/home/AccountActionMenu.tsx
@@ -21,43 +21,50 @@ export function AccountActionMenu({ close }: Props): JSX.Element {
   const clearUser = useUserStore((state) => state.clearUser);
 
   return (
-    <div>
-      <div>{user.email}</div>
-      <div>{user.nickname}</div>
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/home');
-        }}
-      >
-        내 패널 모아보기
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          navigate('/account');
-        }}
-      >
-        내 계정 관리
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          logoutMutation.mutate(undefined, {
-            onSuccess: () => {
-              clearAccessToken();
-              clearUser();
-              navigate('/login');
-            },
-            onError: () => {
-              // TODO: invalid access token 나는 경우 어떻게 처리할지 생각하기
-              // onSuccess랑 똑같이 하거나 아니면 토스트 메시지, 모달 띄워주기
-            },
-          });
-        }}
-      >
-        로그아웃
-      </button>
+    <div className="flex flex-col divide-y divide-grey-light">
+      <div className="flex flex-col px-5 py-4">
+        <div className="font-medium ">{user.email}</div>
+        <div className="text-grey-dark">{user.nickname}</div>
+      </div>
+      <div className="flex flex-col py-1">
+        <button
+          className="px-5 py-2 hover:bg-grey-lighter text-left"
+          type="button"
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          내 패널 모아보기
+        </button>
+        <button
+          className="px-5 py-2 hover:bg-grey-lighter text-left"
+          type="button"
+          onClick={() => {
+            navigate('/account');
+          }}
+        >
+          내 계정 관리
+        </button>
+        <button
+          className="px-5 py-2 hover:bg-grey-lighter text-left"
+          type="button"
+          onClick={() => {
+            logoutMutation.mutate(undefined, {
+              onSuccess: () => {
+                clearAccessToken();
+                clearUser();
+                navigate('/login');
+              },
+              onError: () => {
+                // TODO: invalid access token 나는 경우 어떻게 처리할지 생각하기
+                // onSuccess랑 똑같이 하거나 아니면 토스트 메시지, 모달 띄워주기
+              },
+            });
+          }}
+        >
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 }
