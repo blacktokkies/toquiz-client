8e9b16e779d2ebf29e8cbc8b96a45cc89cf2298d
leegwae <leegwae@gmail.com>
2023-08-11 01:13:44 +0900

    feat(home-page): 내 패널 모아보기 버튼 누르면 홈 페이지로 이동한다 (#302)

--- src/components/home/AccountActionMenu.tsx
+++ src/components/home/AccountActionMenu.tsx
@@ -2,11 +2,15 @@ import type { CreateOverlayContentProps } from '@/hooks/useOverlay';
 
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { useUserStore } from '@/hooks/stores/useUserStore';
 
 type Props = CreateOverlayContentProps;
 
 export function AccountActionMenu({ close }: Props): JSX.Element {
+  const navigate = useNavigate();
+
   const user = useUserStore(({ email, nickname }) => ({
     email,
     nickname,
@@ -16,6 +20,14 @@ export function AccountActionMenu({ close }: Props): JSX.Element {
     <div>
       <div>{user.email}</div>
       <div>{user.nickname}</div>
+      <button
+        type="button"
+        onClick={() => {
+          navigate('/home');
+        }}
+      >
+        내 패널 모아보기
+      </button>
     </div>
   );
 }
