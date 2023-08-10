8bc7e81ca21da9bc19aa8148520c3d5724845336
leegwae <leegwae@gmail.com>
2023-08-10 23:11:41 +0900

    design(create-panel): 패널 생성 모달 디자인 적용한다 (#293)

--- src/components/home/CreatePanelModal.tsx
+++ src/components/home/CreatePanelModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Button } from '@/components/system/Button';
 import { LabelInput } from '@/components/system/LabelInput';
 import { useCreatePanelMutation } from '@/hooks/queries/panel';
 import { useForm } from '@/hooks/useForm';
@@ -38,9 +39,13 @@ export function CreatePanelModal({ close }: Props): JSX.Element {
   });
 
   return (
-    <div>
-      <h2>패널 생성하기</h2>
-      <form aria-label="패널 생성" {...formProps}>
+    <div className="flex flex-col p-5 gap-4">
+      <h2 className="font-medium text-xl">패널 생성하기</h2>
+      <form
+        className="flex flex-col gap-5"
+        aria-label="패널 생성"
+        {...formProps}
+      >
         <LabelInput
           label="패널 제목"
           required
@@ -51,26 +56,29 @@ export function CreatePanelModal({ close }: Props): JSX.Element {
         />
         <LabelInput
           label="패널 설명"
-          required
           placeholder="패널 설명을 입력하세요"
           aria-label="패널 설명 인풋"
           errorMessage={errors.description}
           {...inputProps.description}
         />
-        <button
-          type="button"
-          onClick={() => {
-            close();
-          }}
-        >
-          취소
-        </button>
-        <button
-          type="submit"
-          disabled={hasError || createPanelMutation.isLoading}
-        >
-          패널 생성
-        </button>
+        <div className="flex gap-3 justify-end">
+          <Button
+            type="button"
+            className="text-grey-dark bg-white hover:bg-grey-lighter font-normal"
+            onClick={() => {
+              close();
+            }}
+          >
+            취소
+          </Button>
+          <Button
+            type="submit"
+            disabled={hasError || createPanelMutation.isLoading}
+            className="px-4 py-2"
+          >
+            패널 생성
+          </Button>
+        </div>
       </form>
     </div>
   );
