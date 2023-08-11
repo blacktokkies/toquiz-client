1c3200ccf8e181352abc0c843fdaa71ab02e676b
leegwae <leegwae@gmail.com>
2023-08-11 17:33:11 +0900

    feat(home-page): 더보기 아이콘 누르면 액션 메뉴 보여준다 (#314)

--- src/components/home/PanelItem.tsx
+++ src/components/home/PanelItem.tsx
@@ -2,6 +2,10 @@ import type { Panel } from '@/lib/api/panel';
 
 import React from 'react';
 
+import { More } from '@/components/vectors/More';
+
+import { OpenActionMenuArea } from '../system/OpenActionMenuArea';
+
 interface Props {
   panel: Panel;
 }
@@ -21,6 +25,16 @@ export const PanelItem = ({ panel }: Props): JSX.Element => {
         <span className="text-grey-dark text-sm">
           {formatDateString(createdAt)}
         </span>
+        <OpenActionMenuArea
+          ariaLabel="패널 액션 메뉴"
+          open={({ close }) => <>패널 액션 메뉴</>}
+        >
+          <button type="button" aria-label="더보기">
+            <div role="img" aria-label="더보기 아이콘">
+              <More />
+            </div>
+          </button>
+        </OpenActionMenuArea>
         <span className="mt-3 text-grey-dark text-sm truncate hover:whitespace-normal">
           {description}
         </span>
--- src/components/home/__tests__/PanelItem.test.tsx
+++ src/components/home/__tests__/PanelItem.test.tsx
@@ -1,16 +1,37 @@
+import type { Panel } from '@/lib/api/panel';
+
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import { PanelItem, formatDateString } from '@/components/home/PanelItem';
+import { PanelItem } from '@/components/home/PanelItem';
 import { myPanelList } from '@/mocks/data/panel';
 
-describe('PanelItem', () => {
-  it('패널 제목과 만든 날짜를 렌더링한다', () => {
-    const { container } = render(<PanelItem panel={myPanelList[0]} />);
+const panel: Panel = {
+  id: faker.datatype.uuid(),
+  author: 'test@email.com',
+  title: '테스트 패널 제목',
+  description: '테스트 패널 설명',
+  createdAt: new Date().toDateString(),
+  updatedAt: new Date().toDateString(),
+};
 
-    const { title, createdAt } = myPanelList[0];
-    expect(container).toHaveTextContent(title);
-    expect(container).toHaveTextContent(formatDateString(createdAt));
+describe('PanelItem', () => {
+  it('패널 제목과 설명을 렌더링한다', () => {
+    render(<PanelItem panel={panel} />);
+
+    expect(screen.getByText(/패널 제목/)).toBeInTheDocument();
+    expect(screen.getByText(/패널 설명/)).toBeInTheDocument();
+  });
+
+  it('더보기 아이콘을 누르면 패널 액션 메뉴를 보여준다', async () => {
+    render(<PanelItem panel={myPanelList[0]} />);
+
+    const moreButton = screen.getByRole('button', { name: /더보기/ });
+    await userEvent.click(moreButton);
+
+    expect(screen.getByRole('dialog', { name: '패널 액션 메뉴' }));
   });
 });
