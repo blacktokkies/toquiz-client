e6d6b2b510311a65096bbcf434184a9c7d4e2a03
leegwae <leegwae@gmail.com>
2023-08-11 18:10:48 +0900

    feat(update-panel): 패널 수정하기 폼이 초기값을 보여준다 (#316)

--- src/components/home/UpdatePanelActionModal.tsx
+++ src/components/home/UpdatePanelActionModal.tsx
@@ -1,11 +1,74 @@
 import type { CreateOverlayContentProps } from '@/hooks/useOverlay';
+import type { Panel } from '@/lib/api/panel';
 
 import React from 'react';
 
-type Props = CreateOverlayContentProps;
-export function UpdatePanelActionModal({ close }: Props): JSX.Element {
+import { Button } from '@/components/system/Button';
+import { LabelInput } from '@/components/system/LabelInput';
+import { useForm } from '@/hooks/useForm';
+import { isPanelDescription, isPanelTitle } from '@/lib/validator';
+
+type Props = CreateOverlayContentProps & {
+  panel: Panel;
+};
+
+export function UpdatePanelActionModal({ close, panel }: Props): JSX.Element {
+  const { title, description } = panel;
+  const { inputProps, errors, formProps, hasError } = useForm({
+    inputConfigs: {
+      title: {
+        validate: (value) => isPanelTitle(value),
+        errorMessage: '패널 제목은 3 ~ 40자이어야 합니다',
+      },
+      description: {
+        validate: (value) => isPanelDescription(value),
+        errorMessage: '패널 설명은 50자 이하여야 합니다',
+      },
+    },
+    formConfig: {},
+  });
+
   return (
     <div className="flex flex-col p-7 gap-4">
+      <form
+        className="flex flex-col gap-3"
+        aria-label="패널 수정"
+        {...formProps}
+      >
+        <LabelInput
+          id="panel-title"
+          label="패널 제목"
+          required
+          placeholder="패널 제목을 입력하세요"
+          aria-label="패널 제목 인풋"
+          errorMessage={errors.title}
+          {...inputProps.title}
+          defaultValue={title}
+        />
+        <LabelInput
+          id="panel-description"
+          label="패널 설명"
+          placeholder="패널 설명을 입력하세요"
+          aria-label="패널 설명 인풋"
+          errorMessage={errors.description}
+          {...inputProps.description}
+          defaultValue={description}
+        />
+      </form>
+      <div className="flex gap-3 justify-end">
+        <Button
+          type="button"
+          variant="secondary"
+          onClick={() => {
+            close();
+          }}
+        >
+          취소
+        </Button>
+        <Button type="submit" disabled={hasError} onClick={formProps.onSubmit}>
+          패널 취소
+        </Button>
+      </div>
       <h2 className="font-medium text-lg">패널 수정하기</h2>
     </div>
   );
