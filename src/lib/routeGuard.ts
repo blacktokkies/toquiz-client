91ba6e3e4e19344e386871fa68b11a37061301b2
leegwae <leegwae@gmail.com>
2023-08-12 23:02:46 +0900

    comment(route-guard): 함수 설명 주석의 오타를 수정한다

--- src/lib/routeGuard.ts
+++ src/lib/routeGuard.ts
@@ -4,8 +4,8 @@ import { setUserState } from '@/stores/user-store';
 
 /**
  * ## 액세스 토큰 검증 및 재발급
- * 1. 현재 인메모리에 가지고 있는 액세스 토큰으로 `GET /api/user` 요청
- * 2. Unauthorized Error 발생 시 현재 쿠키에 가지고 있는 리프레쉬 토큰으로 `POST /api/users/auth/refresh` 요청
+ * 1. 현재 인메모리에 가지고 있는 액세스 토큰으로 `내 정보 가져오기 API` 요청
+ * 2. Unauthorized Error 발생 시 현재 쿠키에 가지고 있는 리프레쉬 토큰으로 `리프레쉬 API` 요청
  * 3. Unauthorized Error 발생 시 사용자는 다시 로그인을 해야 액세스 토큰을 발급받을 수 있다.
  */
 async function checkAndRefreshAccessToken(): Promise<void> {
