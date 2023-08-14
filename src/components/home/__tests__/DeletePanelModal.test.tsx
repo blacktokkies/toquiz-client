8787d68bbedfaf2495d0446c86be56598d7b8992
leegwae <leegwae@gmail.com>
2023-08-14 11:57:58 +0900

    feat(delete-panel): 패널 삭제하기 헤딩을 보여준다 (#324)

--- src/components/home/__tests__/DeletePanelModal.test.tsx
+++ src/components/home/__tests__/DeletePanelModal.test.tsx
@@ -8,6 +8,11 @@ import { DeletePanelModal } from '../DeletePanelModal';
 const handleClose = vi.fn();
 
 describe('DeletePanelModal', () => {
+  it('패널 삭제하기 헤딩을 보여준다', () => {
+    setup();
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/패널 삭제하기/);
+  });
   it('취소 버튼을 누르면 close 함수가 호출된다', async () => {
     const { closeButton } = setup();
     await userEvent.click(closeButton);
