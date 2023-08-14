2cdedbd1be96aee2041bd991a734368c4f9d49a3
leegwae <leegwae@gmail.com>
2023-08-14 10:19:51 +0900

    feat(home-page): 패널 삭제 버튼을 누르면 패널 삭제 모달이 열린다 (#325)

--- src/components/home/PanelActionMenu.tsx
+++ src/components/home/PanelActionMenu.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 
 type Props = CreateOverlayContentProps & {
   onUpdatePanelButtonClick: () => void;
+  onDeleteButtonClick: () => void;
 };
 
 export function PanelActionMenu({
   close,
   onUpdatePanelButtonClick,
+  onDeleteButtonClick,
 }: Props): JSX.Element {
   return (
     <div className="flex flex-col">
@@ -19,6 +21,13 @@ export function PanelActionMenu({
           onClick={onUpdatePanelButtonClick}
         >
           패널 수정하기
+        </button>
+        <button
+          className="px-5 py-2 hover:bg-grey-lighter text-left"
+          type="button"
+          onClick={onDeleteButtonClick}
+        >
+          패널 삭제하기
         </button>
       </div>
     </div>
--- src/components/home/__tests__/PanelActionMenu.test.tsx
+++ src/components/home/__tests__/PanelActionMenu.test.tsx
@@ -7,6 +7,7 @@ import { PanelActionMenu } from '@/components/home/PanelActionMenu';
 
 const handleClose = vi.fn();
 const handleUpdatePanel = vi.fn();
+const handleDeletePanelButtonClick = vi.fn();
 
 describe('PanelActionMenu', () => {
   it('패널 수정하기 버튼을 누르면 onUpdatePanelButtonClick을 호출한다', async () => {
@@ -15,21 +16,33 @@ describe('PanelActionMenu', () => {
 
     expect(handleUpdatePanel).toHaveBeenCalled();
   });
+
+  it('패널 삭제 버튼을 누르면 onDeletePanelButtonClick을 호출한다', async () => {
+    const { deleteButton } = setup();
+    await userEvent.click(deleteButton);
+
+    expect(handleDeletePanelButtonClick).toHaveBeenCalled();
+  });
 });
 
 function setup(): {
   updateButton: HTMLElement;
+  deleteButton: HTMLElement;
 } {
   render(
     <PanelActionMenu
       close={handleClose}
       onUpdatePanelButtonClick={handleUpdatePanel}
+      onDeleteButtonClick={handleDeletePanelButtonClick}
     />,
   );
 
   const updateButton = screen.getByRole('button', {
     name: /패널 수정하기/,
   });
+  const deleteButton = screen.getByRole('button', {
+    name: /패널 삭제하기/,
+  });
 
-  return { updateButton };
+  return { updateButton, deleteButton };
 }
