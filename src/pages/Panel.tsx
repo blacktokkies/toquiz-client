b7921d812d081cc15c2ffddb80e99aed36ae5590
leegwae <leegwae@gmail.com>
2023-08-15 16:49:43 +0900

    feat(panel-page): 패널 페이지 로더에서 패널 정보 가져오기 API를 호출한다 (#342)

---
+++ src/pages/Panel.tsx
@@ -0,0 +1,14 @@
+import type { LoaderFunction } from 'react-router-dom';
+
+import React from 'react';
+
+import { getPanel } from '@/lib/api/panel';
+
+export const panelLoader: LoaderFunction = ({ params }) => {
+  if (!params.id?.length) return null;
+  getPanel(params.id);
+  return null;
+};
+export function Panel(): JSX.Element {
+  return <div>패널</div>;
+}
