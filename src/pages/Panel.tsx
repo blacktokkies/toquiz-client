d22cb1554759662530fb2d105dc18fce9e2ef44e
leegwae <leegwae@gmail.com>
2023-08-15 19:53:07 +0900

    design(panel-page): 패널 에러 페이지 디자인 적용한다 (#342)

--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -1,17 +1,87 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-throw-literal */
+
 import type { LoaderFunction } from 'react-router-dom';
 
 import React from 'react';
 
-import { getPanel } from '@/lib/api/panel';
+import { clsx } from 'clsx';
+import {
+  useRouteError,
+  json,
+  isRouteErrorResponse,
+  Link,
+} from 'react-router-dom';
 
+import { Logo } from '@/components/vectors';
+import { getPanel } from '@/lib/api/panel';
+import { ApiError } from '@/lib/apiClient';
+
+// [NOTE] 로더 성공 반환값은 any 혹은 null로 고정한다
+// [NOTE] 로더 실패 반환값은 `Response`로 고정한다
 export const panelLoader: LoaderFunction = async ({ params }) => {
-  if (!params.id?.length) return null;
-  await getPanel(params.id);
-  return null;
+  const panelId = params.id!;
+  try {
+    const panel = await getPanel(panelId);
+    return panel;
+  } catch (error) {
+    if (error instanceof ApiError) {
+      /* eslint-disable-next-line @typescript-eslint/no-unsafe-argument */
+      throw json(
+        { ...error.data, id: panelId },
+        {
+          status: error.response.status,
+        },
+      );
+    }
+    throw error;
+  }
 };
+
 export function PanelError(): JSX.Element {
-  return <div>패널 에러 페이지</div>;
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      /* eslint-disable-next-line */
+      const panelId = error.data.id;
+      return (
+        <div className="w-full h-full bg-gradient-to-r from-gray-50 to-slate-100">
+          <div
+            className={clsx(
+              'flex flex-col gap-4 md:gap-7 justify-center items-center pt-40 md:h-full md:pt-0',
+              'xs:flex-row',
+            )}
+          >
+            <div className="flex flex-col gap-3">
+              <h1 className="text-xl font-bold md:text-3xl">
+                이런! 존재하지 않는 패널입니다
+              </h1>
+              <div className="text-grey-dark">
+                <p>
+                  <span className="font-semibold">[{panelId}]</span> 패널을 찾을
+                  수 없습니다
+                </p>
+                새로고침하거나{' '}
+                <Link to="/home" className="underline font-semibold">
+                  홈 페이지
+                </Link>
+                로 돌아가세요
+              </div>
+            </div>
+            <Link to="/home" className="underline font-semibold">
+              <div role="img" aria-label="toquiz 로고">
+                <Logo className="h-20 w-20 md:h-40 md:w-40" />
+              </div>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  return <div>알 수 없는 오류가 발생했습니다</div>;
 }
+
 export function Panel(): JSX.Element {
   return <div>패널 페이지</div>;
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,39 +11,39 @@ module.exports = {
       md: '768px',
       xl: '1200px',
     },
-    colors: {
-      backdrop: 'rgba(36, 36, 36, 0.45)',
-      white: '#FFFFFF',
-      black: '#000000',
-      'off-white': '#F8F8F8',
-      primary: {
-        light: '#ABE1FF',
-        DEFAULT: '#2DB3FF',
-        hover: '#2698D9',
-        dark: '#0C3045',
-      },
-      danger: {
-        DEFAULT: '#E83333',
-        hover: '#C52B2B',
-      },
-      green: {
-        light: '#E2FEED',
-        DEFAULT: '#198E48',
-      },
-      grey: {
-        lighter: '#F5F5F5', // white-hover
-        light: '#ECE3E3', // border
-        DEFAULT: '#B3B3B3', // placeholder
-        dark: '#656060', // text-secondary
-        darkest: '#242424', // text-primary
-      },
-    },
     extend: {
       boxShadow: {
         item: 'rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px',
         'item-hover':
           'rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px',
         '3xl': '0px 30px 90px rgba(0, 0, 0, 0.4)',
+      },
+      colors: {
+        backdrop: 'rgba(36, 36, 36, 0.45)',
+        white: '#FFFFFF',
+        black: '#000000',
+        'off-white': '#F8F8F8',
+        primary: {
+          light: '#ABE1FF',
+          DEFAULT: '#2DB3FF',
+          hover: '#2698D9',
+          dark: '#0C3045',
+        },
+        danger: {
+          DEFAULT: '#E83333',
+          hover: '#C52B2B',
+        },
+        green: {
+          light: '#E2FEED',
+          DEFAULT: '#198E48',
+        },
+        grey: {
+          lighter: '#F5F5F5', // white-hover
+          light: '#ECE3E3', // border
+          DEFAULT: '#B3B3B3', // placeholder
+          dark: '#656060', // text-secondary
+          darkest: '#242424', // text-primary
+        },
       },
     },
   },
