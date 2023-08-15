66759fcd273aa9ff62e38bf7a6d74919d870371e
leegwae <leegwae@gmail.com>
2023-08-15 17:05:44 +0900

    feat(panel-page): 로더가 실패하면 Fallback UI를 보여준다 (#342)

--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -4,11 +4,14 @@ import React from 'react';
 
 import { getPanel } from '@/lib/api/panel';
 
-export const panelLoader: LoaderFunction = ({ params }) => {
+export const panelLoader: LoaderFunction = async ({ params }) => {
   if (!params.id?.length) return null;
-  getPanel(params.id);
+  await getPanel(params.id);
   return null;
 };
+export function PanelError(): JSX.Element {
+  return <div>패널 에러 페이지</div>;
+}
 export function Panel(): JSX.Element {
   return <div>패널 페이지</div>;
 }
--- src/pages/__tests__/panel-route.test.tsx
+++ src/pages/__tests__/panel-route.test.tsx
@@ -4,10 +4,13 @@ import React from 'react';
 
 import { faker } from '@faker-js/faker';
 import { render, screen, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
+import { apiUrl } from '@/lib/api/consts';
 import * as panelApis from '@/lib/api/panel';
-import { Panel, panelLoader } from '@/pages/Panel';
+import { server } from '@/mocks/server';
+import { Panel, panelLoader, PanelError } from '@/pages/Panel';
 
 describe('/panel/:id route', () => {
   it('로더에서 패널 정보 가져오기 API를 호출한다', () => {
@@ -42,6 +45,37 @@ describe('/panel/:id route', () => {
     render(<RouterProvider router={router} />);
     await waitFor(() => {
       expect(screen.getByText(/패널 페이지/)).toBeInTheDocument();
+    });
+  });
+
+  it('패널 정보 가져오기 API가 실패 응답을 반환하면 Fallback UI를 보여준다', async () => {
+    const panelId = faker.datatype.uuid();
+    server.use(
+      rest.get(apiUrl.panel.get(panelId), async (req, res, ctx) =>
+        res(
+          ctx.status(404),
+          ctx.json({
+            code: 'NOT_EXIST_PANEL',
+            statusCode: 404,
+            message: '해당 패널이 존재하지 않습니다.',
+          }),
+        ),
+      ),
+    );
+    const routes: RouteObject[] = [
+      {
+        path: '/panel/:id',
+        element: <Panel />,
+        loader: panelLoader,
+        errorElement: <PanelError />,
+      },
+    ];
+    const router = createMemoryRouter(routes, {
+      initialEntries: [`/panel/${panelId}`],
+    });
+    render(<RouterProvider router={router} />);
+    await waitFor(() => {
+      expect(screen.getByText(/패널 에러 페이지/)).toBeInTheDocument();
     });
   });
 });
