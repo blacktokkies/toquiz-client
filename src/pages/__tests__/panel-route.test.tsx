e6f996a1ca456de5c33f9b842b37d39385cc4e14
leegwae <leegwae@gmail.com>
2023-08-15 16:58:32 +0900

    feat(panel-page): 로더 성공하면 패널 페이지를 보여준다 (#342)

--- src/pages/__tests__/panel-route.test.tsx
+++ src/pages/__tests__/panel-route.test.tsx
@@ -3,7 +3,7 @@ import type { RouteObject } from 'react-router-dom';
 import React from 'react';
 
 import { faker } from '@faker-js/faker';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 import * as panelApis from '@/lib/api/panel';
@@ -25,5 +25,23 @@ describe('/panel/:id route', () => {
     });
     render(<RouterProvider router={router} />);
     expect(spyOnGetPanel).toBeCalledWith(panelId);
+  });
+
+  it('패널 정보 가져오기 API가 성공 응답을 반환하면 패널 페이지를 보여준다', async () => {
+    const panelId = faker.datatype.uuid();
+    const routes: RouteObject[] = [
+      {
+        path: '/panel/:id',
+        element: <Panel />,
+        loader: panelLoader,
+      },
+    ];
+    const router = createMemoryRouter(routes, {
+      initialEntries: [`/panel/${panelId}`],
+    });
+    render(<RouterProvider router={router} />);
+    await waitFor(() => {
+      expect(screen.getByText(/패널 페이지/)).toBeInTheDocument();
+    });
   });
 });
