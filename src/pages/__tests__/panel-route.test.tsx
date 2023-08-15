2da4551886d5fbc64fbaf9bd99957aa9053153a8
leegwae <leegwae@gmail.com>
2023-08-15 17:12:04 +0900

    refactor(panel-route): 중복 로직 함수로 분리한다 (#342)

--- src/pages/__tests__/panel-route.test.tsx
+++ src/pages/__tests__/panel-route.test.tsx
@@ -1,3 +1,4 @@
+import type { ErrorResponse } from '@/lib/api/types';
 import type { RouteObject } from 'react-router-dom';
 
 import React from 'react';
@@ -12,70 +13,59 @@ import * as panelApis from '@/lib/api/panel';
 import { server } from '@/mocks/server';
 import { Panel, panelLoader, PanelError } from '@/pages/Panel';
 
+const panelId = faker.datatype.uuid();
+
 describe('/panel/:id route', () => {
   it('로더에서 패널 정보 가져오기 API를 호출한다', () => {
     const spyOnGetPanel = vi.spyOn(panelApis, 'getPanel');
-    const panelId = faker.datatype.uuid();
-    const routes: RouteObject[] = [
-      {
-        path: '/panel/:id',
-        element: <Panel />,
-        loader: panelLoader,
-      },
-    ];
-    const router = createMemoryRouter(routes, {
-      initialEntries: [`/panel/${panelId}`],
-    });
-    render(<RouterProvider router={router} />);
+    setup();
+
     expect(spyOnGetPanel).toBeCalledWith(panelId);
   });
 
   it('패널 정보 가져오기 API가 성공 응답을 반환하면 패널 페이지를 보여준다', async () => {
-    const panelId = faker.datatype.uuid();
-    const routes: RouteObject[] = [
-      {
-        path: '/panel/:id',
-        element: <Panel />,
-        loader: panelLoader,
-      },
-    ];
-    const router = createMemoryRouter(routes, {
-      initialEntries: [`/panel/${panelId}`],
-    });
-    render(<RouterProvider router={router} />);
+    setup();
+
     await waitFor(() => {
       expect(screen.getByText(/패널 페이지/)).toBeInTheDocument();
     });
   });
 
   it('패널 정보 가져오기 API가 실패 응답을 반환하면 Fallback UI를 보여준다', async () => {
-    const panelId = faker.datatype.uuid();
-    server.use(
-      rest.get(apiUrl.panel.get(panelId), async (req, res, ctx) =>
-        res(
-          ctx.status(404),
-          ctx.json({
-            code: 'NOT_EXIST_PANEL',
-            statusCode: 404,
-            message: '해당 패널이 존재하지 않습니다.',
-          }),
-        ),
-      ),
-    );
-    const routes: RouteObject[] = [
-      {
-        path: '/panel/:id',
-        element: <Panel />,
-        loader: panelLoader,
-        errorElement: <PanelError />,
-      },
-    ];
-    const router = createMemoryRouter(routes, {
-      initialEntries: [`/panel/${panelId}`],
+    overrideGetPanelResponseWithError({
+      code: 'NOT_EXIST_PANEL',
+      statusCode: 404,
+      message: '해당 패널이 존재하지 않습니다.',
     });
-    render(<RouterProvider router={router} />);
+    setup();
+
     await waitFor(() => {
       expect(screen.getByText(/패널 에러 페이지/)).toBeInTheDocument();
     });
   });
 });
+
+function overrideGetPanelResponseWithError(data: ErrorResponse): void {
+  server.use(
+    rest.get(apiUrl.panel.get(panelId), async (req, res, ctx) =>
+      res(ctx.status(data.statusCode), ctx.json(data)),
+    ),
+  );
+}
+
+function setup(): void {
+  const routes: RouteObject[] = [
+    {
+      path: '/panel/:id',
+      element: <Panel />,
+      loader: panelLoader,
+      errorElement: <PanelError />,
+    },
+  ];
+
+  const router = createMemoryRouter(routes, {
+    initialEntries: [`/panel/${panelId}`],
+  });
+
+  render(<RouterProvider router={router} />);
+}
