32683327a2d6532ed519e13235766b2be36abb49
leegwae <leegwae@gmail.com>
2023-08-16 15:44:12 +0900

    refactor(panel-header): 패널 페이지에서 헤더 컴포넌트로 분리한다 (#353)

---
+++ src/components/panel/__tests__/PanelHeader.test.tsx
@@ -0,0 +1,51 @@
+import type { Panel as PanelData } from '@/lib/api/panel';
+
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+
+import { PanelHeader } from '@/components/panel/PanelHeader';
+import { renderWithAllProviders } from '@/lib/test-utils';
+import { createMockPanel } from '@/mocks/data/panel';
+
+const panel: PanelData = createMockPanel();
+
+describe('패널 페이지 헤더', () => {
+  it('패널 이름을 보여준다', () => {
+    renderWithAllProviders(<PanelHeader panel={panel} />);
+
+    expect(screen.getByRole('heading')).toHaveTextContent(panel.title);
+  });
+
+  it('내 계정 아이콘을 누르면 계정 액션 메뉴를 보여준다', async () => {
+    renderWithAllProviders(
+      <MemoryRouter>
+        <PanelHeader panel={panel} />
+      </MemoryRouter>,
+    );
+
+    const accountButton = screen.getByRole('button', {
+      name: /내 계정/,
+    });
+    await userEvent.click(accountButton);
+
+    expect(screen.getByRole('dialog', { name: /내 계정 액션 메뉴/ }));
+  });
+
+  it('메뉴 아이콘을 누르면 사이드시트를 보여준다', async () => {
+    renderWithAllProviders(
+      <MemoryRouter>
+        <PanelHeader panel={panel} />)
+      </MemoryRouter>,
+    );
+
+    const menuButton = screen.getByRole('button', { name: /메뉴 열기/ });
+    await userEvent.click(menuButton);
+
+    expect(
+      screen.getByRole('complementary', { name: /메뉴/ }),
+    ).toBeInTheDocument();
+  });
+});
--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -14,11 +14,8 @@ import {
   useLoaderData,
 } from 'react-router-dom';
 
-import { AccountActionMenu } from '@/components/home/AccountActionMenu';
-import { OpenActionMenuArea } from '@/components/system/OpenActionMenuArea';
-import { SheetController } from '@/components/system/SheetController';
-import { Logo, Account, Menu } from '@/components/vectors';
-import { useOverlay } from '@/hooks/useOverlay';
+import { PanelHeader } from '@/components/panel/PanelHeader';
+import { Logo } from '@/components/vectors';
 import { getPanel } from '@/lib/api/panel';
 import { ApiError } from '@/lib/apiClient';
 // [NOTE] 로더 성공 반환값은 any 혹은 null로 고정한다
@@ -46,45 +43,10 @@ export const panelLoader: LoaderFunction = async ({ params }) => {
 
 export function Panel(): JSX.Element {
   const panel = useLoaderData() as PanelData;
-  const overlay = useOverlay();
-
-  function handleMenuButtonClick(): void {
-    overlay.open(({ close }) => (
-      <SheetController aria-label="메뉴" close={close}>
-        메뉴
-      </SheetController>
-    ));
-  }
 
   return (
     <>
-      <header>
-        <button
-          type="button"
-          className="hover:bg-grey-light rounded-full p-2"
-          aria-label="메뉴 열기"
-          onClick={handleMenuButtonClick}
-        >
-          <div role="img" aria-label="메뉴 아이콘">
-            <Menu className="fill-grey-darkest" />
-          </div>
-        </button>
-        <h1>{panel.title}</h1>
-        <OpenActionMenuArea
-          aria-label="내 계정 액션 메뉴"
-          open={({ close }) => <AccountActionMenu close={close} />}
-        >
-          <button
-            type="button"
-            className="hover:bg-grey-light rounded-full p-2"
-            aria-label="내 계정 아이콘"
-          >
-            <div role="img" aria-label="내 계정 아이콘">
-              <Account className="fill-grey-darkest" />
-            </div>
-          </button>
-        </OpenActionMenuArea>
-      </header>
+      <PanelHeader panel={panel} />
     </>
   );
 }
