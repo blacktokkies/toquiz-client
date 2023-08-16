90848d06af9eebc595bac6aeec65b81503a9c823
leegwae <leegwae@gmail.com>
2023-08-16 15:02:02 +0900

    feat(panel-page): 헤더에서 패널 이름을 보여준다 (#353)

--- src/pages/__tests__/Panel.test.tsx
+++ src/pages/__tests__/Panel.test.tsx
@@ -1,13 +1,33 @@
+import type { Panel as PanelData } from '@/lib/api/panel';
+
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
+import { createMockPanel } from '@/mocks/data/panel';
 import { Panel } from '@/pages/Panel';
 
+const panel: PanelData = createMockPanel();
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const router = (await importOriginal()) ?? {};
+  return { ...router, useLoaderData: vi.fn(() => panel) };
+});
+
 describe('패널 페이지', () => {
-  it('헤더를 보여준다', () => {
+  it('헤더를 보여준다', async () => {
     render(<Panel />);
 
-    expect(screen.getByRole('banner')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByRole('banner')).toBeInTheDocument();
+    });
+  });
+
+  it('헤더에서 패널 이름을 보여준다', async () => {
+    render(<Panel />);
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading')).toHaveTextContent(panel.title);
+    });
   });
 });
