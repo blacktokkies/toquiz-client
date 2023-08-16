b5cd92441665890050b215e89b71654ed66708ee
leegwae <leegwae@gmail.com>
2023-08-16 16:43:22 +0900

    feat(panel-item): 패널 제목 누르면 `onPanelTitleClick` 함수를 호출한다 (#347)

--- src/components/home/__tests__/PanelItem.test.tsx
+++ src/components/home/__tests__/PanelItem.test.tsx
@@ -17,21 +17,49 @@ const panel: Panel = {
 };
 
 const handleOpenActionMenu = vi.fn();
+const handleTitleClick = vi.fn();
 
 describe('PanelItem', () => {
   it('패널 제목과 설명을 렌더링한다', () => {
-    render(<PanelItem panel={panel} openActionMenu={handleOpenActionMenu} />);
+    render(
+      <PanelItem
+        panel={panel}
+        openActionMenu={handleOpenActionMenu}
+        onPanelTitleClick={handleTitleClick}
+      />,
+    );
 
     expect(screen.getByText(/패널 제목/)).toBeInTheDocument();
     expect(screen.getByText(/패널 설명/)).toBeInTheDocument();
   });
 
   it('더보기 아이콘을 누르면 openActionMenu 함수를 호출한다', async () => {
-    render(<PanelItem panel={panel} openActionMenu={handleOpenActionMenu} />);
+    render(
+      <PanelItem
+        panel={panel}
+        openActionMenu={handleOpenActionMenu}
+        onPanelTitleClick={handleTitleClick}
+      />,
+    );
 
     const moreButton = screen.getByRole('button', { name: /더보기/ });
     await userEvent.click(moreButton);
 
     expect(handleOpenActionMenu).toHaveBeenCalled();
+  });
+
+  it('패널 제목을 누르면 onPanelTitleClick 함수를 호출한다', async () => {
+    render(
+      <PanelItem
+        panel={panel}
+        openActionMenu={handleOpenActionMenu}
+        onPanelTitleClick={handleTitleClick}
+      />,
+    );
+
+    const titleButton = screen.getByRole('button', { name: panel.title });
+    await userEvent.click(titleButton);
+
+    expect(handleTitleClick).toHaveBeenCalled();
   });
 });
