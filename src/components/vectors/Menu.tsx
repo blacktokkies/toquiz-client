9f4747f7daf9ceced2e76fa87adae945e1225c5b
leegwae <leegwae@gmail.com>
2023-08-16 17:18:07 +0900

    fix(vector): 메뉴 아이콘 `viewBox` 수정한다

--- src/components/vectors/Menu.tsx
+++ src/components/vectors/Menu.tsx
@@ -6,8 +6,8 @@ export const Menu = (props: SVGProps<SVGSVGElement>): JSX.Element => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     height="24"
+    viewBox="0 -960 960 960"
     width="24"
-    viewBox="0 0 24 24"
     {...props}
   >
     <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
