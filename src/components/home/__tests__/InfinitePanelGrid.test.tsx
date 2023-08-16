ddbba172b9137b7ef90fe330d32aca751c7462b6
leegwae <leegwae@gmail.com>
2023-08-16 16:52:10 +0900

    feat(panel-item): 패널 제목 누르면 `onPanelTitleClick` 함수를 호출한다 (#347)

--- src/components/home/__tests__/InfinitePanelGrid.test.tsx
+++ src/components/home/__tests__/InfinitePanelGrid.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 
 import { renderWithAllProviders } from '@/lib/test-utils';
 
@@ -9,7 +10,11 @@ import { InfinitePanelGrid } from '../InfinitePanelGrid';
 
 describe('InfinitePanelGrid', () => {
   it('패널 생성 버튼을 누르면 패널 생성 모달이 열린다', async () => {
-    renderWithAllProviders(<InfinitePanelGrid />);
+    renderWithAllProviders(
+      <MemoryRouter>
+        <InfinitePanelGrid />
+      </MemoryRouter>,
+    );
 
     await waitFor(() => {
       expect(screen.queryByText(/loading/)).not.toBeInTheDocument();
