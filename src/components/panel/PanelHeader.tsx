b9a23792eaf558160a80b0001c0ef411c8be5aca
leegwae <leegwae@gmail.com>
2023-08-16 17:39:33 +0900

    design(panel-header): 패널 헤더 디자인 적용한다 (#353)

--- src/components/panel/PanelHeader.tsx
+++ src/components/panel/PanelHeader.tsx
@@ -23,32 +23,34 @@ export function PanelHeader({ panel }: Props): JSX.Element {
   }
 
   return (
-    <header>
-      <button
-        type="button"
-        className="hover:bg-grey-light rounded-full p-2"
-        aria-label="메뉴 열기"
-        onClick={handleMenuButtonClick}
-      >
-        <div role="img" aria-label="메뉴 아이콘">
-          <Menu className="fill-grey-darkest" />
-        </div>
-      </button>
-      <h1>{panel.title}</h1>
-      <OpenActionMenuArea
-        aria-label="내 계정 액션 메뉴"
-        open={({ close }) => <AccountActionMenu close={close} />}
-      >
+    <header className="bg-primary-dark shadow-md">
+      <div className="container flex justify-between items-center gap-3 max-w-7xl px-5 h-16">
         <button
           type="button"
-          className="hover:bg-grey-light rounded-full p-2"
-          aria-label="내 계정 아이콘"
+          className="rounded-full p-2"
+          aria-label="메뉴 열기"
+          onClick={handleMenuButtonClick}
         >
-          <div role="img" aria-label="내 계정 아이콘">
-            <Account className="fill-grey-darkest" />
+          <div role="img" aria-label="메뉴 아이콘">
+            <Menu className="fill-white" />
           </div>
         </button>
-      </OpenActionMenuArea>
+        <h1 className="text-white truncate">{panel.title}</h1>
+        <OpenActionMenuArea
+          aria-label="내 계정 액션 메뉴"
+          open={({ close }) => <AccountActionMenu close={close} />}
+        >
+          <button
+            type="button"
+            className="rounded-full p-2"
+            aria-label="내 계정 아이콘"
+          >
+            <div role="img" aria-label="내 계정 아이콘">
+              <Account className="fill-white" />
+            </div>
+          </button>
+        </OpenActionMenuArea>
+      </div>
     </header>
   );
 }
