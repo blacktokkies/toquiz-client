23a6cabbc7ae6f624cd6dbd71d3263b7f9a6629f
leegwae <leegwae@gmail.com>
2023-08-16 18:23:36 +0900

    feat(panel-sidebar): 버튼 누르면 패널 url을 복사한다 (#357)

--- src/components/panel/__tests__/PanelSidebar.test.tsx
+++ src/components/panel/__tests__/PanelSidebar.test.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import type { Panel } from '@/lib/api/panel';
 
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { formatDateString } from '@/components/home/PanelItem';
 import { PanelSidebar } from '@/components/panel/PanelSidebar';
@@ -19,5 +21,23 @@ describe('PanelSidebar', () => {
     expect(
       screen.getByText(formatDateString(panel.createdAt)),
     ).toBeInTheDocument();
+  });
+
+  it('패널 URL 복사하기 버튼을 누르면 사용자가 패널 URL을 복사한다', async () => {
+    render(<PanelSidebar panel={panel} />);
+    const copyUrlButton = screen.getByRole('button', {
+      name: /패널 URL 복사하기/,
+    });
+
+    Object.assign(window.navigator, {
+      clipboard: {
+        writeText: vi.fn(async () => Promise.resolve()),
+      },
+    });
+
+    await userEvent.click(copyUrlButton);
+    expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith(
+      `${window.location.origin}/panel/${panel.id}`,
+    );
   });
 });
