641fa53754d4dcc4470488ed2d98cb1c1f571c88
leegwae <leegwae@gmail.com>
2023-08-18 01:33:01 +0900

    feat(panel-error): 패널 알 수 없는 에러에는 Fallback UI 보여준다

--- src/pages/Panel.tsx
+++ src/pages/Panel.tsx
@@ -94,5 +94,61 @@ export function PanelError(): JSX.Element {
     }
   }
 
-  return <div>알 수 없는 오류가 발생했습니다</div>;
+  return (
+    <main className="h-full overflow-auto bg-gradient-to-r from-gray-50 to-slate-100">
+      <header className="border-b border-grey-light">
+        <div className="container flex justify-between items-center max-w-7xl px-5 h-16">
+          <Link to="/" className="rounded-md font-bold">
+            toquiz
+          </Link>
+          <div className="flex items-center gap-2">
+            <Link
+              to="/login"
+              className={clsx(
+                'py-2 px-4 rounded-md font-medium',
+                'hover:bg-grey-lighter focus:border-primary-hover ',
+              )}
+            >
+              로그인
+            </Link>
+            <Link
+              to="/signup"
+              className={clsx(
+                'py-2 px-4 rounded-md border border-primary font-medium',
+                'hover:border-primary-hover hover:opacity-70 focus:border-primary-hover',
+              )}
+            >
+              회원가입
+            </Link>
+          </div>
+        </div>
+      </header>
+      <div className="h-36" />
+      <div
+        className={clsx(
+          'container flex flex-col flex-1 items-center gap-4 max-w-xl px-8',
+          'xs:flex-row xs:justify-center',
+          'md:gap-7',
+        )}
+      >
+        <div className="flex flex-col gap-3">
+          <h1 className="text-xl font-bold md:text-3xl">
+            이런! 알 수 없는 오류가 발생했습니다
+          </h1>
+          <div className="text-grey-dark">
+            새로고침하거나{' '}
+            <Link to="/" className="underline font-semibold hover:text-primary">
+              시작 페이지
+            </Link>
+            로 돌아가세요
+          </div>
+        </div>
+        <Link to="/" className="underline font-semibold">
+          <div role="img" aria-label="toquiz 로고">
+            <Logo className="h-20 w-20 md:h-40 md:w-40" />
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
 }
