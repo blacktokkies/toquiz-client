8172f55371f650979fc4e9a4239f76d6bc6e033a
leegwae <leegwae@gmail.com>
2023-08-17 20:46:58 +0900

    feat(index-page): 임시 인덱스 페이지 디자인 적용한다 (#355)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,64 @@
 import React from 'react';
 
+import { clsx } from 'clsx';
 import { Link } from 'react-router-dom';
 
+import { Logo } from '@/components/vectors';
+
 export const Index = (): JSX.Element => (
-  <div>
-    <Link to="/login" className="border">
-      로그인
-    </Link>
-    <Link to="/signup" className="border">
-      회원가입
-    </Link>
-    <Link to="/home" className="border">
-      메인
-    </Link>
+  <div className="h-full w-full bg-off-white">
+    <header className="border-b border-grey-light">
+      <div className="container flex justify-between items-center max-w-7xl px-5 h-16">
+        <Link to="/" className="rounded-md font-bold">
+          toquiz
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link
+            to="/login"
+            className={clsx(
+              'py-2 px-4 rounded-md font-medium',
+              'hover:bg-grey-lighter focus:border-primary-hover ',
+            )}
+          >
+            로그인
+          </Link>
+          <Link
+            to="/signup"
+            className={clsx(
+              'py-2 px-4 rounded-md border border-primary font-medium',
+              'hover:border-primary-hover hover:opacity-70 focus:border-primary-hover',
+            )}
+          >
+            회원가입
+          </Link>
+        </div>
+      </div>
+    </header>
+    <div className="w-full h-36" />
+    <div className="flex flex-col items-center max-w-xl px-5 mx-auto gap-6">
+      <div role="img" aria-label="toquiz 로고">
+        <Logo className="h-24 w-24 sm:h-40 sm:w-40" />
+      </div>
+      <div className="flex flex-col w-full justify-start gap-3">
+        <div>
+          <p className="text-5xl font-bold sm:text-7xl">익명으로 질문을</p>
+          <p className="text-5xl font-bold sm:text-7xl">받아보세요</p>
+        </div>
+        <p className="text-grey-dark sm:text-lg">
+          강의나 세미나를 진행할 때, 혹은 친구 사이에 부담없이 익명으로 질문을
+          올리고 답변을 주고받아요
+        </p>
+      </div>
+      <Link
+        to="/home"
+        className={clsx(
+          'w-full py-3 rounded-md bg-primary text-center font-bold',
+          'hover:bg-primary-hover focus:bg-primary-hover focus:border focus:border-black',
+        )}
+      >
+        이용하기
+      </Link>
+      토퀴즈는 현재 개발 중인 서비스입니다
+    </div>
   </div>
 );
