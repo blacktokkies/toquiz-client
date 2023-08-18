ec0bfa15cf964382cb38818590d67b5daa46d42a
leegwae <leegwae@gmail.com>
2023-08-18 16:40:54 +0900

    fix(label-input): 테스트 오타 수정한다 (#380)

--- src/components/system/__tests__/LabelInput.test.tsx
+++ src/components/system/__tests__/LabelInput.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react';
 import { LabelInput } from '@/components/system/LabelInput';
 
 describe('LabelInput', () => {
-  it('label 옵션을 전달하면 에러 메시지를 보여준다', () => {
+  it('label 옵션에 텍스트를 전달하면 해당 텍스트를 보여준다', () => {
     render(<LabelInput label="라벨" />);
 
     expect(screen.getByText(/라벨/)).toBeInTheDocument();
