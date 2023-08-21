8abf87916370cc5d0c3227494c810c09d0b2b179
leegwae <leegwae@gmail.com>
2023-08-21 18:18:12 +0900

    feat(panel-page): 질문 목록을 보여준다 (#383)

--- src/pages/__tests__/Panel.test.tsx
+++ src/pages/__tests__/Panel.test.tsx
@@ -2,11 +2,13 @@ import type { Panel as PanelData } from '@/lib/api/panel';
 
 import React from 'react';
 
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 
 import { OverlayProvider } from '@/contexts/OverlayContext';
+import * as questionApis from '@/lib/api/question';
 import { renderWithQueryClient } from '@/lib/test-utils';
 import { createMockPanel } from '@/mocks/data/panel';
+import { mockQuestionList } from '@/mocks/data/question';
 import { Panel } from '@/pages/Panel';
 
 const panel: PanelData = createMockPanel();
@@ -17,13 +19,32 @@ vi.mock('react-router-dom', async (importOriginal) => {
 });
 
 describe('패널 페이지', () => {
-  it('헤더를 보여준다', () => {
+  it('헤더를 보여준다', async () => {
     renderWithQueryClient(
       <OverlayProvider>
         <Panel />
       </OverlayProvider>,
     );
 
-    expect(screen.getByRole('banner')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByRole('banner')).toBeInTheDocument();
+    });
+  });
+
+  it('질문 목록 가져오기 API를 호출하고 성공 시 질문 목록을 렌더링한다', async () => {
+    const spyOnGetQuestions = vi.spyOn(questionApis, 'getQuestions');
+    renderWithQueryClient(
+      <OverlayProvider>
+        <Panel />
+      </OverlayProvider>,
+    );
+
+    expect(spyOnGetQuestions).toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(
+        screen.getAllByText(mockQuestionList[0].content)[0],
+      ).toBeInTheDocument();
+    });
   });
 });
