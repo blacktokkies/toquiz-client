a11e2279042ffd11795dad6dc4d23a2105f188c5
leegwae <leegwae@gmail.com>
2023-08-23 15:58:43 +0900

    feat(quesiton-list): 질문 목록 디자인 적용한다 (#383)

--- src/components/panel/InfiniteQuestionList.tsx
+++ src/components/panel/InfiniteQuestionList.tsx
@@ -2,6 +2,7 @@ import type { Panel } from '@/lib/api/panel';
 
 import React, { useCallback } from 'react';
 
+import { Account } from '@/components/vectors';
 import { useQuestionsInfiniteQuery } from '@/hooks/queries/question';
 
 import { IntersectionArea } from '../system/IntersectionArea';
@@ -30,15 +31,27 @@ export function InfiniteQuestionList({ panelId }: Props): JSX.Element {
   const questionPages = questionsQuery.data.pages;
 
   return (
-    <>
-      <ul>
+    <div>
+      <ul className="flex flex-col gap-3">
         {questionPages.map((questionPage) =>
           questionPage.questions.map((question) => (
-            <li key={question.id}>{question.content}</li>
+            <li
+              key={question.id}
+              className="flex flex-col gap-3 p-5 w-full border border-grey-light rounded-md"
+            >
+              <div className="flex justify-start items-center gap-1 overflow-hidden">
+                <div role="img" aria-label="내 계정 아이콘">
+                  <Account className="fill-grey-darkest" />
+                </div>
+                <div className="font-bold whitespace-nowrap">익명</div>
+                <div className="text-grey-dark">{question.createdAt}</div>
+              </div>
+              <div>{question.content}</div>
+            </li>
           )),
         )}
       </ul>
       <IntersectionArea onIntersection={fetchQuestions} />
-    </>
+    </div>
   );
 }
