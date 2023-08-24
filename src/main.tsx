e319efea95a45d90da88adbf791d0b0493cd7826
leegwae <leegwae@gmail.com>
2023-08-24 15:22:50 +0900

    fix: 필요없는 `async` IIFE 블록 삭제한다

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,16 @@ import { App } from '@/App';
 import '@/index.css';
 
 if (import.meta.env.MODE === 'development') {
-  (async () => {
-    const { worker } = await import('@/mocks/browser');
-    worker.start({
-      onUnhandledRequest(req) {
-        console.warn(
-          'Found an unhandled %s request to %s',
-          req.method,
-          req.url.href,
-        );
-      },
-    });
-  })();
+  const { worker } = await import('@/mocks/browser');
+  worker.start({
+    onUnhandledRequest(req) {
+      console.warn(
+        'Found an unhandled %s request to %s',
+        req.method,
+        req.url.href,
+      );
+    },
+  });
 }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
