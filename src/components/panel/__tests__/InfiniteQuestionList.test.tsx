1df10f9cb8048ddbcc0e586a5a78db633ab2a179
leegwae <leegwae@gmail.com>
2023-08-25 01:08:40 +0900

    feat(question-list): 좋아요순 버튼을 누르면 좋아요순으로 질문 목록 보여준다 (#395)

--- src/components/panel/__tests__/InfiniteQuestionList.test.tsx
+++ src/components/panel/__tests__/InfiniteQuestionList.test.tsx
@@ -1,13 +1,16 @@
 import type { Panel } from '@/lib/api/panel';
+import type { InfiniteData } from '@tanstack/react-query';
 
 import React from 'react';
 
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { InfiniteQuestionList } from '@/components/panel/InfiniteQuestionList';
 import * as questionApis from '@/lib/api/question';
-import { renderWithQueryClient } from '@/lib/test-utils';
+import { queryKey } from '@/lib/queryKey';
+import { createQueryClient, renderWithQueryClient } from '@/lib/test-utils';
 import { createMockQuestionList } from '@/mocks/data/question';
 import { overrideGetQuestionsWithSuccess } from '@/pages/__tests__/Panel.test';
 
@@ -46,9 +49,15 @@ describe('InfiniteQuestionList', () => {
     expect(spyOnGetQuestions).toHaveBeenCalledTimes(2);
   });
 
-  it('최신순 버튼을 누르면 최신순으로 질문 목록 API를 호출한다', async () => {
-    const spyOnGetQuestions = vi.spyOn(questionApis, 'getQuestions');
-    renderWithQueryClient(<InfiniteQuestionList panelId={panelId} />);
+  it('최신순 버튼을 누르면 최신순으로 질문 목록을 보여준다', async () => {
+    const queryClient = createQueryClient();
+    render(<InfiniteQuestionList panelId={panelId} />, {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      ),
+    });
 
     await waitFor(() => {
       expect(screen.queryByText(/loading/)).not.toBeInTheDocument();
@@ -57,9 +66,46 @@ describe('InfiniteQuestionList', () => {
     const recentButton = screen.getByRole('button', { name: '최신순' });
     await userEvent.click(recentButton);
 
-    expect(spyOnGetQuestions).toHaveBeenCalledWith(panelId, {
-      page: 0,
-      sort: 'createdDate,DESC',
+    await waitFor(() => {
+      const queryData = queryClient.getQueryData<
+        InfiniteData<questionApis.GetQuestionsResult>
+      >(queryKey.question.list(panelId, 'createdDate,DESC'));
+
+      expect(
+        /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain */
+        screen.getByText(queryData?.pages[0].questions[0].content!),
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('최신순 버튼 눌렀다가 좋아요순 버튼 누르면 좋아요순으로 질문 목록을 보여준다', async () => {
+    const queryClient = createQueryClient();
+    render(<InfiniteQuestionList panelId={panelId} />, {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      ),
+    });
+
+    await waitFor(() => {
+      expect(screen.queryByText(/loading/)).not.toBeInTheDocument();
+    });
+
+    const recentButton = screen.getByRole('button', { name: '최신순' });
+    await userEvent.click(recentButton);
+    const popularButton = screen.getByRole('button', { name: '좋아요순' });
+    await userEvent.click(popularButton);
+
+    await waitFor(() => {
+      const queryData = queryClient.getQueryData<
+        InfiniteData<questionApis.GetQuestionsResult>
+      >(queryKey.question.list(panelId));
+
+      expect(
+        /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain */
+        screen.getByText(queryData?.pages[0].questions[0].content!),
+      ).toBeInTheDocument();
     });
   });
 });
