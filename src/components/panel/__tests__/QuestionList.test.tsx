a995b7c54ec1b6b32ad7cbba3662f2c2dd0451a0
leegwae <leegwae@gmail.com>
2023-08-25 17:01:20 +0900

    design(question-list): 토글할 때마다 좋아요 버튼 활성화/비활성화한다 (#410)

--- src/components/panel/__tests__/QuestionList.test.tsx
+++ src/components/panel/__tests__/QuestionList.test.tsx
@@ -1,4 +1,5 @@
-import type * as questionApis from '@/lib/api/question';
+import type { MyActiveInfo } from '@/lib/api/active-Info';
+import type { QuestionPage } from '@/lib/api/question';
 
 import React from 'react';
 
@@ -6,16 +7,19 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { renderWithQueryClient } from '@/lib/test-utils';
-import { myActiveInfoMock } from '@/mocks/data/active-info';
-import { createMockQuestionList } from '@/mocks/data/question';
+import {
+  createMockQuestion,
+  createMockQuestionList,
+} from '@/mocks/data/question';
 
 import { QuestionList } from '../QuestionList';
 
 const handleLikeButtonClick = vi.fn();
 const handleLikeButtonClickFn = vi.fn(() => handleLikeButtonClick);
+
 describe('QuestionList', () => {
   it('좋아요 버튼을 누를 때마다 onLikeButtonClick이 반환하는 함수를 호출한다', async () => {
-    const questionPages: questionApis.QuestionPage[] = [
+    const questionPages: QuestionPage[] = [
       {
         questions: createMockQuestionList(3),
         nextPage: -1,
@@ -26,12 +30,60 @@ describe('QuestionList', () => {
       <QuestionList
         onLikeButtonClick={handleLikeButtonClickFn}
         questionPages={questionPages}
-        likeIds={myActiveInfoMock.likedIds}
+        likeIds={[]}
       />,
     );
 
     const likeButton = screen.getAllByRole('button', { name: /좋아요 버튼/ });
     await userEvent.click(likeButton[0]);
     expect(handleLikeButtonClick).toHaveBeenCalled();
+  });
+
+  describe('likeIds', () => {
+    it('likeIds에 속한 질문은 좋아요 버튼이 활성화되어있다', () => {
+      const question = createMockQuestion();
+      const likeIds = [question.id];
+      const questionPages: QuestionPage[] = [
+        {
+          questions: [question],
+          nextPage: -1,
+        },
+      ];
+
+      renderWithQueryClient(
+        <QuestionList
+          onLikeButtonClick={handleLikeButtonClickFn}
+          questionPages={questionPages}
+          likeIds={likeIds}
+        />,
+      );
+
+      const likeButton = screen.getByRole('button', { name: /좋아요 버튼/ });
+      expect(likeButton).toHaveAttribute('aria-pressed', 'true');
+    });
+
+    it('likeIds에 속하지 않은 질문은 좋아요 버튼이 비활성화되어있다', () => {
+      const question = createMockQuestion();
+      const likeIds: MyActiveInfo['likedIds'] = [];
+      const questionPages: QuestionPage[] = [
+        {
+          questions: [question],
+          nextPage: -1,
+        },
+      ];
+
+      renderWithQueryClient(
+        <QuestionList
+          onLikeButtonClick={handleLikeButtonClickFn}
+          questionPages={questionPages}
+          likeIds={likeIds}
+        />,
+      );
+
+      const likeButton = screen.getByRole('button', {
+        name: /좋아요 버튼/,
+      });
+      expect(likeButton).toHaveAttribute('aria-pressed', 'false');
+    });
   });
 });
