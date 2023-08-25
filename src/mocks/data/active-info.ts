878f30c6d35c9ed3a07778f3872bfbfb58d6afe6
leegwae <leegwae@gmail.com>
2023-08-25 21:54:43 +0900

    chore(active-info-mock): 활동 정보 mock 데이터 수정한다

--- src/mocks/data/active-info.ts
+++ src/mocks/data/active-info.ts
@@ -1,12 +1,6 @@
 import type { MyActiveInfo } from '@/lib/api/active-Info';
-import type { Question } from '@/lib/api/question';
-
-import { mockQuestionList } from '@/mocks/data/question';
 
 export const myActiveInfoMock: MyActiveInfo = {
-  createdIds: [1, 2, 3],
-  likedIds: mockQuestionList.reduce<Array<Question['id']>>((acc, cur) => {
-    if (acc.length === 10 || cur.likeNum === 0) return acc;
-    return [...acc, cur.id];
-  }, []),
+  createdIds: [],
+  likedIds: [],
 };
