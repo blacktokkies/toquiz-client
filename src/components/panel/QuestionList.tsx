1d724ee39259113a26e87a003c2277803bc0fd54
leegwae <leegwae@gmail.com>
2023-08-25 17:01:20 +0900

    design(question-list): 토글할 때마다 좋아요 버튼 활성화/비활성화한다 (#410)

--- src/components/panel/QuestionList.tsx
+++ src/components/panel/QuestionList.tsx
@@ -31,39 +31,43 @@ export function QuestionList({
   return (
     <ul className="flex flex-col gap-3">
       {questionPages.map((questionPage) =>
-        questionPage.questions.map((question) => (
-          <li
-            key={question.id}
-            className="flex flex-col gap-3 p-5 w-full border border-grey-light rounded-md"
-          >
-            <div className="flex items-center justify-between">
-              <div className="flex justify-start items-center gap-1 overflow-hidden">
-                <div role="img" aria-label="내 계정 아이콘">
-                  <Account className="fill-grey-darkest" />
+        questionPage.questions.map((question) => {
+          const isActived = likeSet.has(question.id);
+          return (
+            <li
+              key={question.id}
+              className="flex flex-col gap-3 p-5 w-full border border-grey-light rounded-md"
+            >
+              <div className="flex items-center justify-between">
+                <div className="flex justify-start items-center gap-1 overflow-hidden">
+                  <div role="img" aria-label="내 계정 아이콘">
+                    <Account className="fill-grey-darkest" />
+                  </div>
+                  <div className="font-bold whitespace-nowrap">익명</div>
+                  <div className="text-grey-dark">
+                    {formatTimeDifference(now, new Date(question.createdAt))}
+                  </div>
                 </div>
-                <div className="font-bold whitespace-nowrap">익명</div>
-                <div className="text-grey-dark">
-                  {formatTimeDifference(now, new Date(question.createdAt))}
-                </div>
+                <button
+                  aria-pressed={isActived}
+                  className={clsx(
+                    'flex itesm-center gap-2 py-1 px-2 rounded-2xl border-2 text-sm',
+                    isActived
+                      ? 'border-green font-bold text-green bg-green-light fill-green hover:bg-green-lighter active:opacity-80'
+                      : 'border-grey-light text-grey-dark fill-grey-dark hover:bg-grey-lighter active:opacity-80',
+                  )}
+                  type="button"
+                  onClick={onLikeButtonClick(question)}
+                >
+                  <Like className="w-5 h-5" />
+                  <span className="sr-only">좋아요 버튼</span>
+                  {question.likeNum}
+                </button>
               </div>
-              <button
-                className={clsx(
-                  'flex itesm-center gap-2 py-1 px-2 rounded-2xl border-2 text-sm',
-                  likeSet.has(question.id)
-                    ? 'border-green font-bold text-green bg-green-light fill-green hover:bg-green-lighter active:opacity-80'
-                    : 'border-grey-light text-grey-dark fill-grey-dark hover:bg-grey-lighter active:opacity-80',
-                )}
-                type="button"
-                onClick={onLikeButtonClick(question)}
-              >
-                <Like className="w-5 h-5" />
-                <span className="sr-only">좋아요 버튼</span>
-                {question.likeNum}
-              </button>
-            </div>
-            <div>{question.content}</div>
-          </li>
-        )),
+              <div>{question.content}</div>
+            </li>
+          );
+        }),
       )}
     </ul>
   );
