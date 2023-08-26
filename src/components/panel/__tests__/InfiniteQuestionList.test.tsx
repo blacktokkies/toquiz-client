a3a9af8abbb0c49b46c75988d8526a237b55dbe6
leegwae <leegwae@gmail.com>
2023-08-26 17:55:08 +0900

    refactor: 필요없는 `expect` 문, `waitFor`문을 삭제한다

--- src/components/panel/__tests__/InfiniteQuestionList.test.tsx
+++ src/components/panel/__tests__/InfiniteQuestionList.test.tsx
@@ -1,6 +1,6 @@
 import type { Panel } from '@/lib/api/panel';
 import type { ErrorResponse } from '@/lib/api/types';
-import type { InfiniteData, QueryClient } from '@tanstack/react-query';
+import type { QueryClient } from '@tanstack/react-query';
 
 import React from 'react';
 
@@ -11,29 +11,23 @@ import { rest } from 'msw';
 import { InfiniteQuestionList } from '@/components/panel/InfiniteQuestionList';
 import { apiUrl } from '@/lib/api/apiUrl';
 import * as questionApis from '@/lib/api/question';
-import { queryKey } from '@/lib/queryKey';
 import { renderWithQueryClient } from '@/lib/test-utils';
 import { delay } from '@/lib/test-utils/delay';
 import { createMockQuestion } from '@/mocks/data/question';
 import { server } from '@/mocks/server';
-import { overrideGetQuestionsWithSuccess } from '@/pages/__tests__/Panel.test';
 
 const panelId: Panel['id'] = 0;
 
 describe('InfiniteQuestionList', () => {
   describe('질문 목록 렌더링', () => {
     it('질문 목록 가져오기 API를 호출하고 성공 시 질문 목록을 렌더링한다', async () => {
-      const question = { ...createMockQuestion(), content: '안녕하세요' };
-      overrideGetQuestionsWithSuccess({
-        statusCode: 200,
-        result: { questions: [question], nextPage: -1 },
-      });
+      const { questions } = overrideGetQuestionsWithSingleQuestion();
       const spyOnGetQuestions = vi.spyOn(questionApis, 'getQuestions');
       setup();
 
       expect(spyOnGetQuestions).toHaveBeenCalled();
       await waitFor(() => {
-        expect(screen.getByText(/안녕하세요/)).toBeInTheDocument();
+        expect(screen.getByText(questions[0].content)).toBeInTheDocument();
       });
     });
 
@@ -49,64 +43,53 @@ describe('InfiniteQuestionList', () => {
   });
 
   describe('질문 목록 정렬', () => {
-    it('최신순 버튼을 누르면 최신순으로 질문 목록을 보여준다', async () => {
-      const { queryClient, waitForFinish } = setup();
+    it('최신순 버튼을 누르면 최신순으로 질문 목록 API 호출한다', async () => {
+      const { waitForFinish } = setup();
       await waitForFinish();
 
+      const spyOnGetQuestions = vi.spyOn(questionApis, 'getQuestions');
       const recentButton = screen.getByRole('button', { name: '최신순' });
       await userEvent.click(recentButton);
 
-      await waitFor(() => {
-        const queryData = queryClient.getQueryData<
-          InfiniteData<questionApis.GetQuestionsResult>
-        >(queryKey.question.list(panelId, 'createdDate,DESC'));
+      expect(spyOnGetQuestions).toHaveBeenCalledWith(panelId, {
+        page: 0,
+        sort: 'createdDate,DESC',
+      });
+    });
 
-        expect(
-          /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain */
-          screen.getByText(queryData?.pages[0].questions[0].content!),
-        ).toBeInTheDocument();
+    it('좋아요순 버튼 누르면 좋아요순으로 질문 목록 API 호출한다', async () => {
+      const { waitForFinish } = setup();
+      await waitForFinish();
+
+      const spyOnGetQuestions = vi.spyOn(questionApis, 'getQuestions');
+      const recentButton = screen.getByRole('button', { name: '최신순' });
+      await userEvent.click(recentButton);
+      const popularButton = screen.getByRole('button', { name: '좋아요순' });
+      await userEvent.click(popularButton);
+
+      expect(spyOnGetQuestions).toHaveBeenCalledWith(panelId, {
+        page: 0,
       });
     });
   });
 
-  it('최신순 버튼 눌렀다가 좋아요순 버튼 누르면 좋아요순으로 질문 목록을 보여준다', async () => {
-    const { queryClient, waitForFinish } = setup();
-    await waitForFinish();
-
-    const recentButton = screen.getByRole('button', { name: '최신순' });
-    await userEvent.click(recentButton);
-    const popularButton = screen.getByRole('button', { name: '좋아요순' });
-    await userEvent.click(popularButton);
-
-    await waitFor(() => {
-      const queryData = queryClient.getQueryData<
-        InfiniteData<questionApis.GetQuestionsResult>
-      >(queryKey.question.list(panelId));
-
-      expect(
-        /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain */
-        screen.getByText(queryData?.pages[0].questions[0].content!),
-      ).toBeInTheDocument();
-    });
-  });
-
   describe('좋아요 버튼', () => {
-    it('좋아요 버튼을 누를 때마다 좋아요 API 요청한다', async () => {
-      const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
+    it('좋아요 버튼을 누르면 질문 좋아요 API 요청한다', async () => {
       const { waitForFinish } = setup();
       await waitForFinish();
 
+      const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
       const likeButton = screen.getAllByRole('button', { name: /좋아요 / })[0];
       await userEvent.click(likeButton);
 
       expect(spyOnLikeQuestion).toHaveBeenCalled();
     });
 
-    it('좋아요 버튼을 누를 때마다 좋아요 API 응답을 기다리지 않고 화면에 토글 결과를 보여준다', async () => {
-      const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
+    it('이때 좋아요 API 응답을 기다리지 않고 화면에 토글 결과를 보여준다', async () => {
       const { waitForFinish } = setup();
       await waitForFinish();
 
+      const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
       const likeButton = screen.getAllByRole('button', { name: /좋아요 / })[0];
       const prevLikeNum = Number(likeButton.textContent);
       await userEvent.click(likeButton);
@@ -123,7 +106,6 @@ describe('InfiniteQuestionList', () => {
           statusCode: 400,
           message: '유효하지 않은 좋아요 활성화 요청입니다.',
         });
-        const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
         const { waitForFinish } = setup();
         await waitForFinish();
 
@@ -131,8 +113,6 @@ describe('InfiniteQuestionList', () => {
         const prevLikeNum = Number(likeButton.textContent);
         await userEvent.click(likeButton);
 
-        expect(spyOnLikeQuestion).toHaveBeenCalled();
-        expect(likeButton).toHaveTextContent(String(prevLikeNum + 1));
         await delay(300);
         await waitFor(() => {
           expect(likeButton).toHaveTextContent(String(prevLikeNum));
@@ -156,20 +136,14 @@ describe('InfiniteQuestionList', () => {
           );
         }),
       );
-      const spyOnLikeQuestion = vi.spyOn(questionApis, 'likeQuestion');
-      const { waitForFinish, queryClient } = setup();
+      const { waitForFinish } = setup();
       await waitForFinish();
 
       const likeButton = screen.getByRole('button', { name: /좋아요 / });
       await userEvent.click(likeButton);
 
-      expect(spyOnLikeQuestion).toHaveBeenCalled();
-      await waitFor(() => {
-        expect(queryClient.isMutating()).toBe(0);
-      });
-      await waitFor(() => {
-        expect(likeButton).not.toBeInTheDocument();
-      });
+      await delay(300);
+      expect(likeButton).not.toBeInTheDocument();
     });
   });
 });
@@ -202,7 +176,7 @@ export function overrideLikeQuestionWithError(data: ErrorResponse): void {
 function overrideGetQuestionsWithSingleQuestion(): {
   questions: questionApis.Question[];
 } {
-  const questions = [createMockQuestion()];
+  const questions = [{ ...createMockQuestion(), content: '안녕하세요' }];
   server.use(
     rest.get(apiUrl.question.getQuestions(':panelId'), async (req, res, ctx) =>
       res(
