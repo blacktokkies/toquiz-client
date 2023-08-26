7948132b45c9a841341e9d66462312e1d2ce61b0
leegwae <leegwae@gmail.com>
2023-08-26 15:11:08 +0900

    refactor: 사용하지 않는 쿼리 삭제한다

--- src/hooks/queries/active-info.ts
+++ src/hooks/queries/active-info.ts
@@ -1,23 +1,18 @@
-import type {
-  GetMyActiveInfoResult,
-  MyActiveInfo,
-} from '@/lib/api/active-Info';
+import type { MyActiveInfo } from '@/lib/api/active-Info';
 import type { Panel } from '@/lib/api/panel';
 import type { ApiError } from '@/lib/apiClient';
 import type { NonNullableKeys } from '@/lib/types';
 import type { FetchQueryOptions } from '@tanstack/react-query';
 import type { Syntax } from 'postcss';
 
-import { useQueryClient } from '@tanstack/react-query';
-
 import { getMyActiveInfo } from '@/lib/api/active-Info';
 import { queryKey } from '@/lib/queryKey';
 
 type _ActiveInfoDetailQueryOptions = Pick<
   FetchQueryOptions<
-    GetMyActiveInfoResult,
+    MyActiveInfo,
     ApiError | Syntax,
-    GetMyActiveInfoResult,
+    MyActiveInfo,
     ReturnType<typeof queryKey.activeInfo.detail>
   >,
   'queryFn' | 'queryKey' | 'staleTime'
@@ -33,14 +28,3 @@ export const activeInfoDetailQuery = (
   queryFn: async () => getMyActiveInfo(panelId).then((res) => res.result),
   staleTime: Infinity,
 });
-
-export const useActiveInfoDeatilQueryData = (
-  panelId: Panel['id'],
-): MyActiveInfo | undefined => {
-  const queryClient = useQueryClient();
-  const activeInfo = queryClient.getQueryData<MyActiveInfo>(
-    activeInfoDetailQuery(panelId).queryKey,
-  );
-
-  return activeInfo;
-};
