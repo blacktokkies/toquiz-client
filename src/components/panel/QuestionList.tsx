100ac6ed2895f7d6ab9aa6b55ed15ba425c70ba8
leegwae <leegwae@gmail.com>
2023-08-26 18:39:38 +0900

    refactor(format-date): 날짜 포매팅하는 함수 `lib` 로 분리한다 (#408)

--- src/components/panel/QuestionList.tsx
+++ src/components/panel/QuestionList.tsx
@@ -4,16 +4,10 @@ import type { Question, QuestionPage } from '@/lib/api/question';
 import React from 'react';
 
 import { clsx } from 'clsx';
-import {
-  differenceInMinutes,
-  differenceInHours,
-  differenceInDays,
-  differenceInWeeks,
-  differenceInMonths,
-} from 'date-fns';
 
 import { Account, Like } from '@/components/vectors';
 import { useCurrentDate } from '@/hooks/useCurrentDate';
+import { formatDistance } from '@/lib/format-date';
 
 interface Props {
   questionPages: QuestionPage[];
@@ -45,7 +39,7 @@ export function QuestionList({
                   </div>
                   <div className="font-bold whitespace-nowrap">익명</div>
                   <div className="text-grey-dark">
-                    {formatTimeDifference(now, new Date(question.createdAt))}
+                    {formatDistance(now, new Date(question.createdAt))}
                   </div>
                 </div>
                 <button
@@ -72,28 +66,4 @@ export function QuestionList({
       )}
     </ul>
   );
-}
-
-/**
- * 날짜 차이를 계산한다
- * @param now 현재
- * @param target 과거
- * @returns 차이가 1분 이내이면 방금, 1시간 이내이면 n분 전, 하루 이내이면 n시간 전, 일주일 이내이면 n일 전, 한 달 이내이면 n주 전, 이후로는 n달 전
- */
-export function formatTimeDifference(now: Date, target: Date): string {
-  const minutesDiff = differenceInMinutes(now, target);
-
-  if (minutesDiff === 0) return '방금';
-  if (minutesDiff < 60) return `${minutesDiff}분 전`;
-
-  const hoursDiff = differenceInHours(now, target);
-  if (hoursDiff < 24) return `${hoursDiff}시간 전`;
-
-  const daysDiff = differenceInDays(now, target);
-  if (daysDiff < 7) return `${daysDiff}일 전`;
-
-  const weeksDiff = differenceInWeeks(now, target);
-  const monthsDiff = differenceInMonths(now, target);
-  if (monthsDiff < 2) return `${weeksDiff}주 전`;
-  return `${monthsDiff}달 전`;
 }
