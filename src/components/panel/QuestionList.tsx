5de8dcfb74c14f851269daf0bd9bc4a35be1e2c3
leegwae <leegwae@gmail.com>
2023-08-28 20:45:00 +0900

    feat(question-list): 질문에 답변이 없으면 답변 개수 보여주지 않는다 (#422)

--- src/components/panel/QuestionList.tsx
+++ src/components/panel/QuestionList.tsx
@@ -78,7 +78,9 @@ export function QuestionList({
                     <div className="text-grey-dark">
                       {formatDistance(now, new Date(question.createdAt))}
                     </div>
-                    <span>답변 {question.answerNum}개</span>
+                    {question.answerNum ? (
+                      <span>답변 {question.answerNum}개</span>
+                    ) : null}
                   </div>
                   <button
                     aria-label={`좋아요 버튼, 좋아요 ${question.likeNum}개`}
