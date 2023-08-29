32d5e01242efff49a6a1da61956abab4eefb9cb5
leegwae <leegwae@gmail.com>
2023-08-29 18:55:26 +0900

    chore(auth-mock): 사용자 정보 mock 데이터 만드는 함수 정의한다

--- src/mocks/data/auth.ts
+++ src/mocks/data/auth.ts
@@ -8,3 +8,13 @@ export const myAccount: Omit<Member, 'password'> = {
   createdAt: new Date().toString(),
   updatedAt: new Date().toString(),
 };
+
+let id = 0;
+export const createMockUser = (): Omit<Member, 'password'> => ({
+  id: id++,
+  email: '테스트 이메일',
+  nickname: '테스트 닉네임',
+  provider: 'TEST',
+  createdAt: new Date().toString(),
+  updatedAt: new Date().toString(),
+});
