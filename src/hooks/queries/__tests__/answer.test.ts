af98a31aae1a8625f10ce09d343b216ea6fd12fd
leegwae <leegwae@gmail.com>
2023-08-29 22:12:01 +0900

    feat(answer-api): 답변 생성 API 함수 호출하는 뮤테이션 작성한다 (#434)

--- src/hooks/queries/__tests__/answer.test.ts
+++ src/hooks/queries/__tests__/answer.test.ts
@@ -1,8 +1,12 @@
 import type { Question } from '@/lib/api/question';
 
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
-import { useAnswersQuery } from '@/hooks/queries/answer';
+import {
+  useAnswersQuery,
+  useCreateAnswerMutation,
+} from '@/hooks/queries/answer';
+import * as answerApis from '@/lib/api/answer';
 import { createQueryClientWrapper } from '@/lib/test-utils';
 
 const questionId: Question['id'] = -1;
@@ -17,5 +21,23 @@ describe('answer API queries', () => {
     });
 
     expect(result.current.data?.id).toBe(questionId);
+  });
+
+  describe('useCreateAnswerMutation', () => {
+    it('mutate를 호출하면 createAnswer가 호출된다', async () => {
+      const spyOnCreateAnswer = vi.spyOn(answerApis, 'createAnswer');
+      const content = '안녕하세요';
+      const { result } = renderHook(() => useCreateAnswerMutation(questionId), {
+        wrapper: createQueryClientWrapper(),
+      });
+
+      act(() => {
+        result.current.mutate('안녕하세요');
+      });
+
+      await waitFor(() => {
+        expect(spyOnCreateAnswer).toHaveBeenCalledWith(questionId, content);
+      });
+    });
   });
 });
--- src/hooks/queries/answer.ts
+++ src/hooks/queries/answer.ts
@@ -1,11 +1,15 @@
-import type { GetAnswersResult } from '@/lib/api/answer';
+import type {
+  Answer,
+  CreateAnswerResult,
+  GetAnswersResult,
+} from '@/lib/api/answer';
 import type { Question } from '@/lib/api/question';
 import type { ApiError } from '@/lib/apiClient';
-import type { UseQueryResult } from '@tanstack/react-query';
+import type { UseMutationResult, UseQueryResult } from '@tanstack/react-query';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
-import { getAnswers } from '@/lib/api/answer';
+import { createAnswer, getAnswers } from '@/lib/api/answer';
 import { queryKey } from '@/lib/queryKey';
 
 export const useAnswersQuery = (
@@ -18,4 +22,25 @@ export const useAnswersQuery = (
   );
 
   return query;
+};
+
+export const useCreateAnswerMutation = (
+  questionId: Question['id'],
+): UseMutationResult<
+  CreateAnswerResult,
+  ApiError | SyntaxError,
+  Answer['content']
+> => {
+  const key = queryKey.answer.create();
+  const mutation = useMutation<
+    CreateAnswerResult,
+    ApiError | SyntaxError,
+    Answer['content']
+  >(
+    key,
+    async (content: Answer['content']) =>
+      (await createAnswer(questionId, content)).result,
+  );
+
+  return mutation;
 };
