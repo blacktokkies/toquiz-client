3d98e287ba6664f861f42d4c5501062e2ade4f0c
leegwae <leegwae@gmail.com>
2023-08-30 22:18:12 +0900

    feat(resign-modal): 유효하지 않은 비밀번호 입력하면 에러 메시지 보여준다 (#441)

--- src/components/account/ResignModal.tsx
+++ src/components/account/ResignModal.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import { useForm } from '@/hooks/useForm';
+import { isPassword } from '@/lib/validator';
+
+import { LabelInput } from '../system/LabelInput';
+
 interface Props {
   close: () => void;
 }
 export function ResignModal({ close }: Props): JSX.Element {
+  const { inputProps, errors } = useForm({
+    inputConfigs: {
+      password: {
+        validate: (value) => isPassword(value),
+        errorMessage:
+          '8~20자 이하의 영문 대소문자, 숫자, 특수기호를 입력하세요',
+      },
+    },
+  });
+
   return (
     <div>
       <h2>회원 탈퇴하기</h2>
+      <form>
+        <LabelInput
+          id="password"
+          label="비밀번호"
+          {...inputProps.password}
+          errorMessage={errors.password}
+        />
+      </form>
       <button type="button" onClick={close}>
         취소
       </button>
--- src/components/account/__tests__/ResignModal.test.tsx
+++ src/components/account/__tests__/ResignModal.test.tsx
@@ -1,9 +1,16 @@
+import type * as Vi from 'vitest';
+
 import React from 'react';
 
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { ResignModal } from '@/components/account/ResignModal';
+import { isPassword } from '@/lib/validator';
+
+vi.mock('@/lib/validator', () => ({
+  isPassword: vi.fn(() => true),
+}));
 
 const handleClose = vi.fn();
 describe('ResignModal', () => {
@@ -19,5 +26,19 @@ describe('ResignModal', () => {
     const closeButton = screen.getByRole('button', { name: /취소/ });
     await userEvent.click(closeButton);
     expect(handleClose).toHaveBeenCalled();
+  });
+
+  it('사용자가 유효하지 않은 비밀번호를 입력하면 에러 메시지를 보여준다', async () => {
+    (isPassword as Vi.Mock).mockImplementation(() => false);
+    render(<ResignModal close={handleClose} />);
+
+    const passwordInput = screen.getByLabelText('비밀번호');
+    await userEvent.type(passwordInput, '유효하지 않은 비밀번호');
+
+    expect(
+      screen.getByText(
+        '8~20자 이하의 영문 대소문자, 숫자, 특수기호를 입력하세요',
+      ),
+    ).toBeInTheDocument();
   });
 });
