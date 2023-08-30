b0418f7634c98f7416bcef65ea5ad99c457507c4
leegwae <leegwae@gmail.com>
2023-08-30 23:08:42 +0900

    design(resign-modal): 회원 탈퇴 모달 디자인 적용한다 (#441)

--- src/components/account/ResignModal.tsx
+++ src/components/account/ResignModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
+import { Button } from '@/components/system/Button';
 import { LabelInput } from '@/components/system/LabelInput';
 import { useResignMutation } from '@/hooks/queries/auth';
 import { useForm } from '@/hooks/useForm';
@@ -52,22 +53,36 @@ export function ResignModal({ close }: Props): JSX.Element {
   });
 
   return (
-    <div>
-      <h2>회원 탈퇴하기</h2>
-      <form {...formProps}>
+    <div className="flex flex-col p-7 gap-4">
+      <h2 className="font-medium text-lg">회원 탈퇴하기</h2>
+      <form
+        className="flex flex-col gap-3"
+        aria-label="회원 탈퇴 폼"
+        {...formProps}
+      >
+        <p>회원을 영구 탈퇴하려면 비밀번호를 입력하세요</p>
         <LabelInput
           id="password"
           label="비밀번호"
+          required={true}
+          type="password"
+          placeholder="비밀번호를 입력하세요"
           {...inputProps.password}
           errorMessage={errors.password}
         />
-        <button type="submit" disabled={hasError}>
-          회원 탈퇴
-        </button>
+        <div className="flex gap-3 justify-end items-center">
+          <Button variant="secondary" type="button" onClick={close}>
+            취소
+          </Button>
+          <Button
+            variant="danger"
+            type="submit"
+            disabled={hasError || resignMutation.isLoading}
+          >
+            회원 탈퇴
+          </Button>
+        </div>
       </form>
-      <button type="button" onClick={close}>
-        취소
-      </button>
     </div>
   );
 }
