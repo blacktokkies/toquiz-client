5f9eeb46d51018fa8402e4d5452ce28153a1dab9
leegwae <leegwae@gmail.com>
2023-08-30 17:30:25 +0900

    feat(account-page): 유효하지 않은 닉네임 입력하면 에러 메시지를 보여준다 (#439)

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 
 import { redirect } from 'react-router-dom';
 
+import { LabelInput } from '@/components/system/LabelInput';
+import { useForm } from '@/hooks/useForm';
 import { isUserLoggedIn } from '@/lib/routeGuard';
+import { isNickname } from '@/lib/validator';
 
 export const accountLoader: LoaderFunction = async () => {
   const isLoggedIn = await isUserLoggedIn();
@@ -13,6 +16,14 @@ export const accountLoader: LoaderFunction = async () => {
   return null;
 };
 export function Account(): JSX.Element {
+  const { inputProps, errors } = useForm({
+    inputConfigs: {
+      nickname: {
+        validate: (value) => isNickname(value),
+        errorMessage: '2~20자 이하의 문자를 입력하세요',
+      },
+    },
+  });
   return (
     <div className="flex-1 container flex flex-col gap-11 max-w-5xl px-5 pt-7 pb-16">
       <h1 className="font text-2xl font-medium tracking-tighter md:text-5xl">
@@ -21,7 +32,14 @@ export function Account(): JSX.Element {
       <div className="flex flex-1 flex-col">
         <section>
           <h2>프로필 수정</h2>
-          <form aria-label="프로필 수정 폼" />
+          <form aria-label="프로필 수정 폼">
+            <LabelInput
+              id="nickname"
+              label="닉네임"
+              {...inputProps.nickname}
+              errorMessage={errors.nickname}
+            />
+          </form>
         </section>
       </div>
     </div>
--- src/pages/__tests__/Account.test.tsx
+++ src/pages/__tests__/Account.test.tsx
@@ -1,9 +1,15 @@
+import type * as Vi from 'vitest';
+
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
+import { isNickname } from '@/lib/validator';
 import { Account } from '@/pages/Account';
 
+vi.mock('@/lib/validator', () => ({
+  isNickname: vi.fn(() => true),
+}));
 describe('내 계정 관리 페이지', () => {
   it('내 계정 관리 헤딩을 보여준다.', () => {
     render(<Account />);
@@ -34,6 +40,20 @@ describe('내 계정 관리 페이지', () => {
       });
 
       expect(form).toBeInTheDocument();
+    });
+
+    it('사용자가 유효하지 않은 닉네임을 입력하면 에러 메시지를 보여준다', () => {
+      (isNickname as Vi.Mock).mockImplementation(() => false);
+      render(<Account />);
+
+      const nicknameInput = screen.getByLabelText(/닉네임/);
+      fireEvent.change(nicknameInput, {
+        target: { value: '유효하지 않은 닉네임' },
+      });
+
+      expect(
+        screen.getByText('2~20자 이하의 문자를 입력하세요'),
+      ).toBeInTheDocument();
     });
   });
 });
