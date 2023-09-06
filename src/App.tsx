8c959a565c43cdcbbe62cb9f466180ffbdc38fd9
leegwae <leegwae@gmail.com>
2023-09-07 02:56:15 +0900

    feat(panel-page): 패널 페이지를 socket provider로 감싼다 (#450)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { Client as SocketClient } from '@stomp/stompjs';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
+import Socket from 'sockjs-client';
 
 import { createRouterWithQueryClient } from '@/router';
 
@@ -14,7 +16,19 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createRouterWithQueryClient(queryClient);
+const socketClient = new SocketClient({
+  webSocketFactory: () => new Socket('/wss'),
+  debug: (msg) => {
+    console.log(msg);
+  },
+  onWebSocketClose: () => {
+    // activate 실패로 connection이 close된 경우,
+    // reconnect 시도하지 않도록 deactivate한다.
+    socketClient.deactivate();
+  },
+});
+
+const router = createRouterWithQueryClient(queryClient, socketClient);
 
 export function App(): JSX.Element {
   return (
