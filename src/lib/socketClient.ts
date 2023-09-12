9a6e4413acad31f1eaa2d44098ac1b945e3635d1
leegwae <leegwae@gmail.com>
2023-09-12 21:01:55 +0900

    feat(socket): 소켓 클라이언트를 패널 구독, 전파 로직 포함한 클라이언트로 모듈화한다 (#458)

---
+++ src/lib/socketClient.ts
@@ -0,0 +1,52 @@
+import type { Answer } from '@/lib/api/answer';
+import type { Panel } from '@/lib/api/panel';
+import type { Question } from '@/lib/api/question';
+import type { StompSubscription } from '@stomp/stompjs';
+
+import { Client } from '@stomp/stompjs';
+
+export class SocketClient extends Client {
+  subscribePanel(
+    panelId: Panel['sid'],
+    callback: (event: SocketEvent) => void,
+  ): StompSubscription {
+    return super.subscribe(`/sub/panels/${panelId}`, (message) => {
+      const event = JSON.parse(message.body) as SocketEvent;
+      callback(event);
+    });
+  }
+
+  unsubscribePanel(id: StompSubscription['id']): void {
+    super.unsubscribe(id);
+  }
+
+  publishToPanel<T extends SocketEvent>(panelId: Panel['sid'], event: T): void {
+    super.publish({
+      destination: `/pub/panels/${panelId}`,
+      body: JSON.stringify(event),
+    });
+  }
+}
+
+export type SocketEvent =
+  | CreateQuestionEvent
+  | LikeQuestionEvent
+  | CreateAnswerEvent;
+export interface CreateQuestionEvent {
+  eventType: 'CREATE_QUESTION';
+  data: Question;
+}
+export interface LikeQuestionEvent {
+  eventType: 'LIKE_QUESTION';
+  data: {
+    questionId: Question['id'];
+    likeNum: Question['likeNum'];
+  };
+}
+export interface CreateAnswerEvent {
+  eventType: 'CREATE_ANSWER';
+  data: {
+    questionId: Question['id'];
+    answer: Answer;
+  };
+}
