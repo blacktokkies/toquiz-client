07c890e25d1b1bb34cab0978ccc7c7bd42980fc5
leegwae <leegwae@gmail.com>
2023-09-14 20:14:54 +0900

    feat(active-info-mock): 테스트 종료마다 mock 데이터를 초기화한다 (#413)

--- src/lib/api/__tests__/active-info.test.ts
+++ src/lib/api/__tests__/active-info.test.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 
 import { getMyActiveInfo } from '@/lib/api/active-Info';
 import { apiUrl } from '@/lib/api/apiUrl';
-import { myActiveInfoMock } from '@/mocks/data/active-info';
+import { mockMyActiveInfo } from '@/mocks/data/active-info';
 
 describe('activeInfo api', () => {
   it(`getMyActiveInfo를 호출하면 내 활동 정보 가져오기 API(${apiUrl.activeInfo.get(
@@ -12,6 +12,6 @@ describe('activeInfo api', () => {
   )})`, async () => {
     const panelId: Panel['sid'] = faker.datatype.uuid();
     const res = await getMyActiveInfo(panelId);
-    expect(res.result).toEqual(myActiveInfoMock);
+    expect(res.result).toEqual(mockMyActiveInfo);
   });
 });
