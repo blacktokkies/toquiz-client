493d9ce45930b9856f87c5f41dff326beee12402
leegwae <leegwae@gmail.com>
2023-09-28 20:34:29 +0900

    feat(active-info-query): 쿼리 옵션을 따로 정의한다

--- src/hooks/queries/active-info.ts
+++ src/hooks/queries/active-info.ts
@@ -2,29 +2,29 @@ import type { MyActiveInfo } from '@/lib/api/active-Info';
 import type { Panel } from '@/lib/api/panel';
 import type { ApiError } from '@/lib/apiClient';
 import type { NonNullableKeys } from '@/lib/types';
-import type { FetchQueryOptions } from '@tanstack/react-query';
+import type { UseQueryResult, UseQueryOptions } from '@tanstack/react-query';
 import type { Syntax } from 'postcss';
+
+import { useQuery } from '@tanstack/react-query';
 
 import { getMyActiveInfo } from '@/lib/api/active-Info';
 import { queryKey } from '@/lib/queryKey';
 
-type _ActiveInfoDetailQueryOptions = Pick<
-  FetchQueryOptions<
-    MyActiveInfo,
-    ApiError | Syntax,
-    MyActiveInfo,
-    ReturnType<typeof queryKey.activeInfo.detail>
-  >,
-  'queryFn' | 'queryKey' | 'staleTime'
+/* =================== [ 활동 정보 디테일 쿼리 ] =================== */
+export type ActiveInfoDetailQuery = NonNullableKeys<
+  Pick<
+    UseQueryOptions<MyActiveInfo, ApiError | Syntax>,
+    'queryFn' | 'queryKey' | 'staleTime'
+  >
 >;
-
-export type ActiveInfoDetailQueryOptions =
-  NonNullableKeys<_ActiveInfoDetailQueryOptions>;
-
 export const activeInfoDetailQuery = (
   panelId: Panel['sid'],
-): ActiveInfoDetailQueryOptions => ({
+): ActiveInfoDetailQuery => ({
   queryKey: queryKey.activeInfo.detail(panelId),
   queryFn: async () => getMyActiveInfo(panelId).then((res) => res.result),
   staleTime: Infinity,
 });
+export const useActiveInfoDetailQuery = (
+  panelId: Panel['sid'],
+): UseQueryResult<MyActiveInfo, ApiError | Syntax> =>
+  useQuery(activeInfoDetailQuery(panelId));
