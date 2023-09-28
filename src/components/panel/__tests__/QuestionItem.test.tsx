100c5df100a4a647bdf45c84e6cc06dba332f2a3
leegwae <leegwae@gmail.com>
2023-09-28 23:05:15 +0900

    refactor(question-item): 공통 로직 `setup` 함수로 분리한다

--- src/components/panel/__tests__/QuestionItem.test.tsx
+++ src/components/panel/__tests__/QuestionItem.test.tsx
@@ -11,22 +11,44 @@ import { QuestionItem } from '../QuestionItem';
 
 const handleLikeButtonClick = vi.fn();
 
+function setup({
+  question,
+  isActived,
+  now,
+}: {
+  question: Question;
+  isActived: boolean;
+  now: Date;
+}): {
+  likeButton: HTMLButtonElement;
+} {
+  render(
+    <QuestionItem
+      question={question}
+      isActived={isActived}
+      onLikeButtonClick={handleLikeButtonClick}
+      now={now}
+    />,
+  );
+
+  const likeButton = screen.getByRole<HTMLButtonElement>('button', {
+    name: /좋아요/,
+  });
+
+  return { likeButton };
+}
 describe('QuestionItem', () => {
   it('질문 내용과 좋아요 개수, 답변 개수를 보여준다', () => {
-    const question: Question = {
-      ...createMockQuestion(),
-      content: '안녕하세요',
-      likeNum: 3,
-      answerNum: 4,
-    };
-    render(
-      <QuestionItem
-        question={question}
-        isActived={true}
-        onLikeButtonClick={handleLikeButtonClick}
-        now={new Date()}
-      />,
-    );
+    setup({
+      question: {
+        ...createMockQuestion(),
+        content: '안녕하세요',
+        likeNum: 3,
+        answerNum: 4,
+      },
+      isActived: true,
+      now: new Date(),
+    });
 
     expect(screen.getByText(/안녕하세요/)).toBeInTheDocument();
     expect(screen.getByLabelText(/좋아요 3개/)).toBeInTheDocument();
@@ -34,35 +56,22 @@ describe('QuestionItem', () => {
   });
 
   it('답변이 없으면 답변 개수를 보여주지 않는다', () => {
-    const question: Question = {
-      ...createMockQuestion(),
-
-      answerNum: 0,
-    };
-    render(
-      <QuestionItem
-        question={question}
-        isActived={true}
-        onLikeButtonClick={handleLikeButtonClick}
-        now={new Date()}
-      />,
-    );
+    setup({
+      question: {
+        ...createMockQuestion(),
+        answerNum: 0,
+      },
+      isActived: true,
+      now: new Date(),
+    });
     expect(screen.queryByText(/답변 0개/)).not.toBeInTheDocument();
   });
 
   it('좋아요 버튼을 누르면 onLikeButtonClick이 호출된다', async () => {
-    const question: Question = createMockQuestion();
-    render(
-      <QuestionItem
-        question={question}
-        isActived={true}
-        onLikeButtonClick={handleLikeButtonClick}
-        now={new Date()}
-      />,
-    );
-
-    const likeButton = screen.getByRole<HTMLButtonElement>('button', {
-      name: /좋아요/,
+    const { likeButton } = setup({
+      question: createMockQuestion(),
+      isActived: true,
+      now: new Date(),
     });
     await userEvent.click(likeButton);
 
@@ -70,35 +79,19 @@ describe('QuestionItem', () => {
   });
 
   it('isActived가 true이면 좋아요 버튼이 활성화되어있다', () => {
-    const question: Question = createMockQuestion();
-    render(
-      <QuestionItem
-        question={question}
-        isActived={true}
-        onLikeButtonClick={handleLikeButtonClick}
-        now={new Date()}
-      />,
-    );
-
-    const likeButton = screen.getByRole<HTMLButtonElement>('button', {
-      name: /좋아요/,
+    const { likeButton } = setup({
+      question: createMockQuestion(),
+      isActived: true,
+      now: new Date(),
     });
     expect(likeButton).toHaveAttribute('aria-pressed', 'true');
   });
 
   it('isActived가 false이면 좋아요 버튼이 비활성화되어있다', () => {
-    const question: Question = createMockQuestion();
-    render(
-      <QuestionItem
-        question={question}
-        isActived={false}
-        onLikeButtonClick={handleLikeButtonClick}
-        now={new Date()}
-      />,
-    );
-
-    const likeButton = screen.getByRole<HTMLButtonElement>('button', {
-      name: /좋아요/,
+    const { likeButton } = setup({
+      question: createMockQuestion(),
+      isActived: false,
+      now: new Date(),
     });
     expect(likeButton).toHaveAttribute('aria-pressed', 'false');
   });
