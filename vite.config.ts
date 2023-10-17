ac404daf78a5461af0832dd1c42ba9317d53bb9b
leegwae <leegwae@gmail.com>
2023-10-18 00:12:37 +0900

    build: 번들에서 모듈이 차지하는 크기 시각화하는 플러그인 설정한다 (#481)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -24,6 +25,9 @@ export default defineConfig(({ mode }) => {
       checker({
         typescript: true,
         eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
+      }),
+      visualizer({
+        filename: 'report/stats.html',
       }),
     ],
     server: {
