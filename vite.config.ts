77885f35abd7b052335a08f6073a15f32eeb1a56
leegwae <leegwae@gmail.com>
2023-10-18 00:37:13 +0900

    build: react router 모듈을 청크로 분리한다 (#481)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
@@ -22,6 +22,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tsconfigPaths(),
+      splitVendorChunkPlugin(),
       checker({
         typescript: true,
         eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
@@ -30,6 +31,21 @@ export default defineConfig(({ mode }) => {
         filename: 'report/stats.html',
       }),
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: string) {
+            if (
+              id.includes('react-router-dom') ||
+              id.includes('@remix-run') ||
+              id.includes('react-router')
+            ) {
+              return '@react-router';
+            }
+          },
+        },
+      },
+    },
     server: {
       host: 'localhost',
       port: 5173,
