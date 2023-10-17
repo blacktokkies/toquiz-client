c8a146425f97fe84bb138f3bacac4e57872d19f4
leegwae <leegwae@gmail.com>
2023-10-18 01:04:59 +0900

    Revert "build: 번들에서 모듈이 차지하는 크기 시각화하는 플러그인 설정한다 (#481)"
    This reverts commit 27fa84e9c6142717bfa4b85724f9dd76df263e0b.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -25,9 +24,6 @@ export default defineConfig(({ mode }) => {
       checker({
         typescript: true,
         eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
-      }),
-      visualizer({
-        filename: 'report/stats.html',
       }),
     ],
     server: {
